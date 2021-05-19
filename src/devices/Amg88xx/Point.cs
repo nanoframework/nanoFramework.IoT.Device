1ca4d37fb3c56e8dc7e134aa0d38d3ff31a08f19
Laurent Ellerbach <laurelle@microsoft.com>
2021-05-19 19:30:58 +0200

    Adding Amg8xx

---
+++ src/devices/Amg88xx/Point.cs
@@ -0,0 +1,32 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Iot.Device.Amg88xx
+{
+    /// <summary>
+    /// Simple Point class.
+    /// </summary>
+    public class Point
+    {
+        /// <summary>
+        /// Constructor for Point.
+        /// </summary>
+        /// <param name="x">The x-coordinate.</param>
+        /// <param name="y">The y-coordinate.</param>
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        /// <summary>
+        /// X coordinate
+        /// </summary>
+        public int X { get; set; }
+
+        /// <summary>
+        /// Y coordinate
+        /// </summary>
+        public int Y { get; set; }
+    }
+}
---
+++ src/devices/Amg88xx/TemperatureLowHighBytes.cs
@@ -0,0 +1,17 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Iot.Device.Amg88xx
+{
+    internal class TemperatureLowHighBytes
+    {
+        public TemperatureLowHighBytes(byte low, byte high)
+        {
+            LowByte = low;
+            HighByte = high;
+        }
+
+        public byte LowByte { get; set; }
+        public byte HighByte { get; set; }
+    }
+}
