5a20000bbf98085444461afb4ccbcbbb6bba5369
Laurent Ellerbach <laurelle@microsoft.com>
2021-05-20 16:31:36 +0200

    Add ak8963 basic

---
+++ devices/Ak8963/samples/Program.cs
@@ -0,0 +1,33 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Device.I2c;
+using System.IO;
+using System.Threading;
+using System.Numerics;
+using Iot.Device.Magnetometer;
+
+I2cConnectionSettings mpui2CConnectionSettingmpus = new(1, Ak8963.DefaultI2cAddress);
+using Ak8963 ak8963 = new Ak8963(I2cDevice.Create(mpui2CConnectionSettingmpus));
+Debug.WriteLine(
+    "Magnetometer calibration is taking couple of seconds, move your sensor in all possible directions! Make sure you don't have a magnet or phone close by.");
+Vector3 mag = ak8963.CalibrateMagnetometer();
+Debug.WriteLine($"Bias:");
+Debug.WriteLine($"Mag X = {mag.X}");
+Debug.WriteLine($"Mag Y = {mag.Y}");
+Debug.WriteLine($"Mag Z = {mag.Z}");
+Debug.WriteLine("Press a key to continue");
+Console.ReadKey();
+Console.Clear();
+
+while (!Console.KeyAvailable)
+{
+    Vector3 magne = ak8963.ReadMagnetometer(true, TimeSpan.FromMilliseconds(11));
+    Debug.WriteLine($"Mag X = {magne.X,15}");
+    Debug.WriteLine($"Mag Y = {magne.Y,15}");
+    Debug.WriteLine($"Mag Z = {magne.Z,15}");
+    Thread.Sleep(200);
+}
+
+Console.ReadKey();
