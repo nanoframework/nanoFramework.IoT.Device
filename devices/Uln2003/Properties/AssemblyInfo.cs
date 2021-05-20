194acbcb935d7418d7f36b670877382156183df5
zeier <33636338+zeier@users.noreply.github.com>
2021-05-20 09:28:10 +0200

    Add converted binding for Uln2003 from .NET IoT (#4)
    * Add converted binding for Uln2003 from .NET IoT

---
+++ devices/Uln2003/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Uln203")]
+[assembly: AssemblyCopyright("Copyright (c) .NET Foundation and Contributors")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+/////////////////////////////////////////////////////////////////
+[assembly: AssemblyNativeVersion("0.0.0.0")]
+/////////////////////////////////////////////////////////////////
