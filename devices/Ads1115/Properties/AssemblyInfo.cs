58a51f3f6ce37f9641b1fa6de053ecac528d1a2b
Laurent Ellerbach <laurelle@microsoft.com>
2021-05-20 12:27:21 +0200

    Adding Ads1115

---
+++ devices/Ads1115/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Iot.Device.Ads1115")]
+[assembly: AssemblyCompany("nanoFramework Contributors")]
+[assembly: AssemblyCopyright("Copyright(c).NET Foundation and Contributors")]
+
+[assembly: ComVisible(false)]
+
+/////////////////////////////////////////////////////////////////
+// This attribute is mandatory when building Interop libraries //
+[assembly: AssemblyNativeVersion("0.0.0.0")]
+/////////////////////////////////////////////////////////////////
