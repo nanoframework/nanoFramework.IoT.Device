2f6d3acaad989f9e905e889a8de6311d128b1774
José Simões <jose.simoes@eclo.solutions>
2021-05-20 10:55:59 +0100

    Improvements in tests and samples projects
    - GUIDs are now properly updated in the solution file.
    - Fix adding samples and test projects when they are in general samples and test folders.
    - Fix path to packages in samples and test projects.

--- src/nanoFramework.IoT.Device.CodeConverter/Program.cs
+++ src/nanoFramework.IoT.Device.CodeConverter/Program.cs
@@ -9,6 +9,13 @@ namespace nanoFramework.IoT.Device.CodeConverter
 {
     class Program
     {
+        private const string _unitTestProjectGuidReplacementToken = "<!--UNIT TEST PROJECT GUID GOES HERE-->";
+        private const string _sampleProjectGuidReplacementToken = "<!--SAMPLES PROJECT GUID GOES HERE-->";
+        private const string _outputTypeReplacementToken = "<!-- OUTPUT TYPE -->";
+        private const string _unitTestProjectPlaceholderToken = "<!-- UNIT TESTS PROJECT PLACEHOLDER -->";
+        private const string _sampleProjectPlaceholderToken = "<!-- SAMPLES PROJECT PLACEHOLDER -->";
+
+
         static void Main(string[] args)
         {
             // Initialize configuration.
@@ -39,11 +46,13 @@ namespace nanoFramework.IoT.Device.CodeConverter
                 // check if this a Unit Test project
                 ProjectType projectType = ProjectType.Regular;
 
-                if(sourceProjectFile.DirectoryName.EndsWith("tests"))
+                if(sourceProjectFile.DirectoryName.EndsWith("tests") &&
+                    Directory.GetFiles(sourceProjectFile.DirectoryName, "*.csproj").Length > 0)
                 {
                     projectType = ProjectType.UnitTest;
                 }
-                else if(sourceProjectFile.DirectoryName.EndsWith("samples"))
+                else if(sourceProjectFile.DirectoryName.EndsWith("samples") &&
+                    Directory.GetFiles(sourceProjectFile.DirectoryName, "*.csproj").Length > 0)
                 {
                     projectType = ProjectType.Samples;
                 }
@@ -97,6 +106,14 @@ namespace nanoFramework.IoT.Device.CodeConverter
                     CreateSolutionFile(projectName, targetDirectoryInfo, projectGuid);
                     UpdateSolutionFile(projectName, targetDirectoryInfo, projectGuid);
                 }
+                else
+                {
+                    // fill in project GUID
+                    UpdateProjectGuidInSolutionFile(
+                        targetDirectoryInfo,
+                        projectType,
+                        projectGuid);
+                }
 
                 // NUSPEC File
                 if (projectType == ProjectType.Regular)
@@ -122,6 +139,7 @@ namespace nanoFramework.IoT.Device.CodeConverter
                 targetNuspecFile.MoveTo($"{targetDirectory}\\{projectName}.nuspec", true);
             }
         }
+        
         private static void CreateProjectFile(
             ProjectType projectType,
             string projectName,
@@ -156,7 +174,7 @@ namespace nanoFramework.IoT.Device.CodeConverter
                 // build proper project reference
                 projectReplacements.Add(
                     "<!-- INSERT PROJECT UNDER TEST REFERENCE HERE -->",
-                    $@"<ProjectReference Include=""..\{projectName.Replace(".Tests", "")}\{projectName.Replace(".Tests", "")}.nfproj"" />");
+                    $@"<ProjectReference Include=""..\{projectName.Replace(".Tests", "")}.nfproj"" />");
             }
             else
             {
@@ -185,7 +203,8 @@ namespace nanoFramework.IoT.Device.CodeConverter
             {
                 var newProjectReferencesString = newProjectReferences.Distinct().Aggregate((seed, add) => $"{seed.Replace("$LF$", "\n")}\n    {add.Replace("$LF$", "\n")}");
 
-                if (projectType == ProjectType.UnitTest)
+                if (projectType == ProjectType.UnitTest ||
+                    projectType == ProjectType.Samples)
                 {
                     // add the reference to the project being tested
                     newProjectReferencesString += unitTestProjectReference;
@@ -207,12 +226,12 @@ namespace nanoFramework.IoT.Device.CodeConverter
             if(projectType == ProjectType.Samples)
             {
                 // samples projects are executables
-                projectReplacements.Add("<!-- OUTPUT TYPE -->", "Executable");
+                projectReplacements.Add(_outputTypeReplacementToken, "Executable");
             }
             else
             {
                 // all the others are libraries
-                projectReplacements.Add("<!-- OUTPUT TYPE -->", "Library");
+                projectReplacements.Add(_outputTypeReplacementToken, "Library");
             }
 
             targetProjectFile.EditFile(projectReplacements);
@@ -270,6 +289,7 @@ Microsoft Visual Studio Solution File, Format Version 12.00
 VisualStudioVersion = 16.0.30413.136
 MinimumVisualStudioVersion = 10.0.40219.1
 [[ INSERT PROJECTS HERE ]]
+
 Global
     GlobalSection(SolutionConfigurationPlatforms) = preSolution
         Debug|Any CPU = Debug|Any CPU
@@ -294,22 +314,79 @@ EndProject";
             if(targetDirectoryInfo.GetDirectories("samples").Count() > 0)
             {
                 solutionProject += $@"
-Project(""{{11A8DD76-328B-46DF-9F39-F559912D0360}}"") = ""nanoFrameworkIoT.Samples"", ""samples\nanoFrameworkIoT.Samples.nfproj"", ""{projectGuid}""
+Project(""{{11A8DD76-328B-46DF-9F39-F559912D0360}}"") = ""nanoFrameworkIoT.Samples"", ""samples\nanoFrameworkIoT.Samples.nfproj"", ""{_sampleProjectGuidReplacementToken}""
 EndProject";
+            }
+            else
+            {
+                solutionProject += $@"
+<!-- SAMPLES PROJECT PLACEHOLDER -->";
             }
 
             // find out if there are unit test projects
             if (targetDirectoryInfo.GetDirectories("tests").Count() > 0)
             {
                 solutionProject += $@"
-Project(""{{11A8DD76-328B-46DF-9F39-F559912D0360}}"") = ""nanoFrameworkIoT.Tests"", ""tests\nanoFrameworkIoT.Tests.nfproj"", ""{projectGuid}""
+Project(""{{11A8DD76-328B-46DF-9F39-F559912D0360}}"") = ""nanoFrameworkIoT.Tests"", ""tests\nanoFrameworkIoT.Tests.nfproj"", ""{_unitTestProjectGuidReplacementToken}""
 EndProject";
+            }
+            else
+            {
+                solutionProject += $@"
+<!-- UNIT TESTS PROJECT PLACEHOLDER -->";
             }
 
             var solutionFileContent = solutionFileTemplate.Replace("[[ INSERT PROJECTS HERE ]]", solutionProject);
             solutionFileContent = solutionFileContent.Replace("[[ INSERT BUILD CONFIGURATIONS HERE ]]", solutionBuildConfigTemplate);
             File.WriteAllText($"{targetDirectoryInfo.FullName}\\{projectName}.sln", solutionFileContent);
         }
+
+        private static void UpdateProjectGuidInSolutionFile(
+            DirectoryInfo targetDirectoryInfo,
+            ProjectType projectType,
+            string projectGuid)
+        {
+            // find the parent solution file
+            // it's OK to simplify because there will be only one SLN file there
+            var slnFile = Directory.GetFiles(targetDirectoryInfo.Parent.FullName, "*.sln").FirstOrDefault();
+
+            if (slnFile != null)
+            {
+                // load Solution file content
+                string slnContent = File.ReadAllText(slnFile);
+
+                // replace project GUID
+                if (projectType == ProjectType.Samples)
+                {
+                    slnContent = slnContent.Replace(_sampleProjectGuidReplacementToken, projectGuid);
+                }
+                else if (projectType == ProjectType.UnitTest)
+                {
+                    slnContent = slnContent.Replace(_unitTestProjectGuidReplacementToken, projectGuid);
+                }
+
+                // add project, if not already there
+
+                // find out if there are sample projects
+                if (projectType == ProjectType.Samples)
+                {
+                    slnContent = slnContent.Replace(
+                        _sampleProjectPlaceholderToken,
+                        $@"Project(""{{11A8DD76-328B-46DF-9F39-F559912D0360}}"") = ""{targetDirectoryInfo.Parent.Name}.Samples"", ""samples\{targetDirectoryInfo.Parent.Name}.Samples.nfproj"", ""{projectGuid}""
+EndProject");
+                }
+                else if (projectType == ProjectType.UnitTest)
+                {
+                    slnContent = slnContent.Replace(
+                                  _unitTestProjectPlaceholderToken,
+                                  $@"Project(""{{11A8DD76-328B-46DF-9F39-F559912D0360}}"") = ""{targetDirectoryInfo.Parent.Name}.Tests"", ""tests\{targetDirectoryInfo.Parent.Name}.Tests.nfproj"", ""{projectGuid}""
+EndProject");
+                }
+
+                File.WriteAllText(slnFile, slnContent);
+            }
+        }
+
         private static void UpdateSolutionFile(string projectName, DirectoryInfo targetDirectoryInfo, string projectGuid)
         {
             var solutionFiles = targetDirectoryInfo.GetFiles("*.sln");
@@ -321,7 +398,6 @@ EndProject";
                     {"nanoFrameworkIoT", projectName }
                 });
             }
-            
         }
 
         private static T InitOptions<T>()
