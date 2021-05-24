9a59e7fd5d8448cce8fe44b6383f1dab8289246e
Laurent Ellerbach <laurelle@microsoft.com>
2021-05-24 13:26:12 +0200

    Adding Mbi5027 (#52)

---
+++ devices/Mbi5027/samples/Program.cs
@@ -0,0 +1,65 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Device.Gpio;
+using System.Device.Spi;
+using System.Diagnostics;
+using System.Threading;
+using Iot.Device.Multiplexing;
+
+using Mbi5027 sr = new(Mbi5027PinMapping.Complete);
+
+Debug.WriteLine($"Driver for {nameof(Mbi5027)}");
+Debug.WriteLine($"Register bit length: {sr.BitLength}");
+
+CheckCircuit(sr);
+BinaryCounter(sr);
+CheckCircuit(sr);
+sr.ShiftClear();
+
+void BinaryCounter(ShiftRegister sr)
+{
+    int endValue = 65_536;
+    Debug.WriteLine($"Write 0 through {endValue}");
+    int delay = 20;
+
+    for (int i = 0; i < endValue; i++)
+    {
+        for (int j = (sr.BitLength / 8) - 1; j > 0; j--)
+        {
+            int shift = j * 8;
+            int downShiftedValue = i >> shift;
+            sr.ShiftByte((byte)downShiftedValue, false);
+        }
+
+        sr.ShiftByte((byte)i);
+        Thread.Sleep(delay);       
+    }
+}
+
+void CheckCircuit(Mbi5027 sr)
+{
+    Debug.WriteLine("Checking circuit");
+    sr.EnableDetectionMode();
+
+    int index = sr.BitLength - 1;
+
+    foreach (var value in sr.ReadOutputErrorStatus())
+    {
+        Debug.WriteLine($"Bit {index--}: {value}");
+    }
+
+    sr.EnableNormalMode();
+}
+
+bool IsCanceled(ShiftRegister sr, CancellationTokenSource cancellationSource)
+{
+    if (cancellationSource.IsCancellationRequested)
+    {
+        sr.ShiftClear();
+        return true;
+    }
+
+    return false;
+}
