8237b4d45849085e846fed90e83004d75d928526
Robb Schiefer <rschiefer@live.com>
2021-05-24 07:07:14 -0500

    added vl53l0x

---
+++ devices/Vl53L0X/samples/Program.cs
@@ -0,0 +1,34 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Device.I2c;
+using System.Threading;
+using Iot.Device.Vl53L0X;
+
+Debug.WriteLine("Hello VL53L0X!");
+using Vl53L0X vL53L0X = new(I2cDevice.Create(new I2cConnectionSettings(1, Vl53L0X.DefaultI2cAddress)));
+Debug.WriteLine($"Rev: {vL53L0X.Information.Revision}, Prod: {vL53L0X.Information.ProductId}, Mod: {vL53L0X.Information.ModuleId}");
+Debug.WriteLine($"Offset in µm: {vL53L0X.Information.OffsetMicrometers}, Signal rate fixed 400 µm: {vL53L0X.Information.SignalRateMeasuementFixed400Micrometers}");
+vL53L0X.MeasurementMode = MeasurementMode.Continuous;
+while (!Console.KeyAvailable)
+{
+    try
+    {
+        var dist = vL53L0X.Distance;
+        if (dist != (ushort)OperationRange.OutOfRange)
+        {
+            Debug.WriteLine($"Distance: {dist}");
+        }
+        else
+        {
+            Debug.WriteLine("Invalid data");
+        }
+    }
+    catch (Exception ex)
+    {
+        Debug.WriteLine($"Exception: {ex.Message}");
+    }
+
+    Thread.Sleep(500);
+}
