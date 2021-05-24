7366d14eb786bdd0fb87fcc02f1e85507529d058
Laurent Ellerbach <laurelle@microsoft.com>
2021-05-24 12:57:34 +0200

    Adding Max7219 (#46)

---
+++ devices/Max7219/DeviceIdDigit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Iot.Device.Max7219
+{
+    public class DeviceIdDigit
+    {
+        public DeviceIdDigit(int deviceId, int digit)
+        {
+            DeviceId = deviceId;
+            Digit = digit;
+        }
+
+        public int DeviceId { get; set; }
+        public int Digit { get; set; }
+    }
+}
