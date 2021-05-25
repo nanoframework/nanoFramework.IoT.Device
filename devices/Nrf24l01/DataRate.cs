e912eb2022c9195c6911d64a7f4d83089a7d9eae
Laurent Ellerbach <laurelle@microsoft.com>
2021-05-25 11:40:08 +0200

    Adding Nrf24l01 (#57)

---
+++ devices/Nrf24l01/DataRate.cs
@@ -0,0 +1,21 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Iot.Device.Nrf24l01
+{
+    /// <summary>
+    /// nRF24L01 Send Data Rate
+    /// </summary>
+    public enum DataRate
+    {
+        /// <summary>
+        /// 1Mbps
+        /// </summary>
+        Rate1Mbps = 0x00,
+
+        /// <summary>
+        /// 2Mbps
+        /// </summary>
+        Rate2Mbps = 0x01
+    }
+}
