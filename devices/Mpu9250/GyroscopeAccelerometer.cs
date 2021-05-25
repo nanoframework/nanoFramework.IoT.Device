5d2de471f537f1e6d334ebe1f60fedb444889a24
Laurent Ellerbach <laurelle@microsoft.com>
2021-05-25 11:32:47 +0200

    Adding Mpu9250 (#56)

---
+++ devices/Mpu9250/GyroscopeAccelerometer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Numerics;
+
+namespace Iot.Device.Imu
+{
+    public class GyroscopeAccelerometer
+
+    {
+        public GyroscopeAccelerometer(Vector3 gyro, Vector3 acc)
+        {
+            Gyroscope = gyro;
+            Accelerometer = acc;
+        }
+
+        public Vector3 Gyroscope { get; set; }
+        public Vector3 Accelerometer { get; set; }
+    }
+}
