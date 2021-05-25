e62135393ffdbe9f464efe7ab4f3eef061fea180
Laurent Ellerbach <laurelle@microsoft.com>
2021-05-25 08:49:27 +0200

    Adding RadioReceiver

---
+++ devices/RadioReceiver/samples/Program.cs
@@ -0,0 +1,16 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Device.I2c;
+using System.Threading;
+using Iot.Device.RadioReceiver;
+using UnitsNet;
+
+I2cConnectionSettings settings = new(1, Tea5767.DefaultI2cAddress);
+using I2cDevice device = I2cDevice.Create(settings);
+
+using Tea5767 radio = new Tea5767(device, FrequencyRange.Other, Frequency.FromMegahertz(103.3));
+
+Thread.Sleep(Timeout.Infinite);
+
