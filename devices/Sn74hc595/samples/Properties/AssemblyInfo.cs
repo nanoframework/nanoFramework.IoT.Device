532c6df4cc4a87f52228789ec40ca118d760b023
Laurent Ellerbach <laurelle@microsoft.com>
2021-05-26 12:36:58 +0200

    Adding Sn74hc595, Adjusting ShiftRegister (#66)

---
+++ devices/Sn74hc595/samples/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Iot.Device.Sn74hc595-driver")]
+[assembly: AssemblyCompany("nanoFramework Contributors")]
+[assembly: AssemblyCopyright("Copyright(c).NET Foundation and Contributors")]
+
+[assembly: ComVisible(false)]
+
