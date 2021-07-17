6a16d5d87518582e1240e848566f84a9383510db
Laurent Ellerbach <laurelle@microsoft.com>
2021-07-17 10:03:00 +0300

    adding comments, adjusting nuspec

--- devices/Pn5180/TripletVersion.cs
+++ devices/Pn5180/TripletVersion.cs
@@ -1,9 +1,21 @@
-﻿using System;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
 
 namespace Iot.Device.Pn5180
 {
+    /// <summary>
+    /// Versions of the reader
+    /// </summary>
     public class TripletVersion
     {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="product">Product version</param>
+        /// <param name="firmware">Firmware version</param>
+        /// <param name="eeprom">EEPROM version</param>
         public TripletVersion(Version product, Version firmware, Version eeprom)
         {
             Product = product;
@@ -11,8 +23,19 @@ namespace Iot.Device.Pn5180
             Eeprom = eeprom;
         }
 
+        /// <summary>
+        /// Product version
+        /// </summary>
         public Version Product { get; set; }
+
+        /// <summary>
+        /// Firmware version
+        /// </summary>
         public Version Firmware { get; set; }
+
+        /// <summary>
+        /// EEPROM version
+        /// </summary>
         public Version Eeprom { get; set; }
     }
 }
