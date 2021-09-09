651511d6760c77d473fac83cfa5d65b0db42e6d0
Carlos Sardo <casard@microsoft.com>
2021-09-09 10:51:35 +0200

    Initial commit of Bmm150 solution (based out of existing one)

---
+++ devices/Bmm150/samples/Program.cs
@@ -0,0 +1,30 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Device.I2c;
+using System.Diagnostics;
+using System.Threading;
+using System.Numerics;
+using Iot.Device.Magnetometer;
+
+//I2cConnectionSettings mpui2CConnectionSettingmpus = new(1, Bmm150.DefaultI2cAddress);
+//using Bmm150 ak8963 = new Bmm150(I2cDevice.Create(mpui2CConnectionSettingmpus));
+//Debug.WriteLine(
+//    "Magnetometer calibration is taking couple of seconds, move your sensor in all possible directions! Make sure you don't have a magnet or phone close by.");
+//Vector3 mag = ak8963.CalibrateMagnetometer();
+//Debug.WriteLine($"Bias:");
+//Debug.WriteLine($"Mag X = {mag.X}");
+//Debug.WriteLine($"Mag Y = {mag.Y}");
+//Debug.WriteLine($"Mag Z = {mag.Z}");
+//Debug.WriteLine("Press a key to continue");
+//Thread.Sleep(1000);
+
+while (true)
+{
+    //Vector3 magne = ak8963.ReadMagnetometer(true, TimeSpan.FromMilliseconds(11));
+    //Debug.WriteLine($"Mag X = {magne.X,15}");
+    //Debug.WriteLine($"Mag Y = {magne.Y,15}");
+    //Debug.WriteLine($"Mag Z = {magne.Z,15}");
+    //Thread.Sleep(200);
+}
