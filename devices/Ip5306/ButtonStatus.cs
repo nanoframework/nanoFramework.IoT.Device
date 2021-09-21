56a2162737d613212e4fddc9dd955fd2ab5ea71d
Laurent Ellerbach <laurelle@microsoft.com>
2021-09-21 23:11:00 +0300

    adding IP5306 initial commit

---
+++ devices/Ip5306/ButtonStatus.cs
@@ -0,0 +1,26 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+
+namespace Iot.Device.Ip5306
+{
+    /// <summary>
+    /// Button status
+    /// </summary>
+    [Flags]
+    public enum ButtonStatus
+    {
+        /// <summary>Double click</summary>
+        DoubleClick = 0b0000_0100,
+
+        /// <summary>Long press</summary>
+        LongPress = 0b0000_0010,
+
+        /// <summary>Short press</summary>
+        ShortPress = 0b0000_0001,
+
+        /// <summary>Not pressed</summary>
+        NotPressed = 0b0000_0000,
+    }
+}
