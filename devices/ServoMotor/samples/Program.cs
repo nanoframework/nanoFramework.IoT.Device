d95c57f97aacf42dad29b37ba50c7b756c0bb4ef
Laurent Ellerbach <laurelle@microsoft.com>
2021-09-22 20:35:37 +0200

    Adjust ServoMortor sample (#152)

--- devices/ServoMotor/samples/Program.cs
+++ devices/ServoMotor/samples/Program.cs
@@ -4,15 +4,16 @@
 using System;
 using System.Device.Pwm;
 using System.Diagnostics;
+using System.Threading;
 using Iot.Device.ServoMotor;
 using nanoFramework.Hardware.Esp32;
 
 Debug.WriteLine("Hello Servo Motor!");
 
 // When using an ESP32, you have to setup the pin function then create the PWM channel
-Configuration.SetPinFunction(21, DeviceFunction.PWM1);
+Configuration.SetPinFunction(5, DeviceFunction.PWM1);
 
-using PwmChannel pwmChannel = PwmChannel.CreateFromPin(21, 50);
+using PwmChannel pwmChannel = PwmChannel.CreateFromPin(5, 50);
 using ServoMotor servoMotor = new ServoMotor(
     pwmChannel,
     160,
@@ -27,15 +28,16 @@ int pulseWidthValue;
 int angleValue;
 void WritePulseWidth(PwmChannel pwmChannel, ServoMotor servoMotor)
 {
+    pulseWidthValue = 2200;
     servoMotor.Start();
 
     while (true)
     {
-        Debug.WriteLine("Enter a pulse width in microseconds ('Q' to quit). ");
-        pulseWidthValue = 500;
+        pulseWidthValue = pulseWidthValue == 700 ? 2200 : 700;
 
         servoMotor.WritePulseWidth(pulseWidthValue);
         Debug.WriteLine($"Duty Cycle: {pwmChannel.DutyCycle * 100.0}%");
+        Thread.Sleep(2000);
     }
 
     servoMotor.Stop();
@@ -47,11 +49,11 @@ void WriteAngle(PwmChannel pwmChannel, ServoMotor servoMotor)
 
     while (true)
     {
-        Debug.WriteLine("Enter an angle ('Q' to quit). ");
-        angleValue = 70;
+        angleValue = angleValue == 0 ? 160 : 0;
 
         servoMotor.WriteAngle(angleValue);
         Debug.WriteLine($"Duty Cycle: {pwmChannel.DutyCycle * 100.0}%");
+        Thread.Sleep(2000);
     }
 
     servoMotor.Stop();
