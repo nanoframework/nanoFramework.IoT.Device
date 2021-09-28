ab18bdbfdb5dfd3a62ef3412e6d91dc31957d67c
Martin Tirion <mtirion@live.nl>
2021-09-28 17:05:09 +0200

    Added unit test

--- devices/Button/Button/Tests/UnitTest1.cs
+++ devices/Button/Button/Tests/UnitTest1.cs
@@ -1,6 +1,7 @@
 using Iot.Device.Button;
 using Iot.Device.Tests;
 using nanoFramework.TestFramework;
+using System.Threading;
 
 // DOES NOT WORK YET
 namespace Tests
@@ -9,19 +10,43 @@ namespace Tests
     class Buttontests
     {
         [TestMethod]
-        public void TestOne()
+        public void Test_click()
         {
-
-            TestButton button = new TestButton();
-
             bool IsPressed = false;
 
+            TestButton button = new TestButton();
             button.Click += (sender, e) =>
             {
                 IsPressed = true;
             };
 
             button.PressButton();
+            Thread.Sleep(200);
+            button.ReleaseButton();
+
+            Assert.Equals(IsPressed, false);
+        }
+
+        [TestMethod]
+        public void Test_double_click()
+        {
+            TestButton button = new TestButton();
+            button.IsDoubleClickEnabled = true;
+            bool IsPressed = false;
+
+            button.DoubleClick += (sender, e) =>
+            {
+                IsPressed = true;
+            };
+
+            button.PressButton();
+            Thread.Sleep(200);
+            button.ReleaseButton();
+
+            Thread.Sleep(400);
+
+            button.PressButton();
+            Thread.Sleep(200);
             button.ReleaseButton();
 
             Assert.Equals(IsPressed, true);
