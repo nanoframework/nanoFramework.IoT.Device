05d46af383262872e11006f7fddd3e94b880b4eb
Stephanie Visser <stvi@microsoft.com>
2021-10-04 15:51:33 +0200

    add headers

--- devices/Button/ButtonHoldingState.cs
+++ devices/Button/ButtonHoldingState.cs
@@ -1,4 +1,7 @@
-﻿namespace Iot.Device.Button
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Iot.Device.Button
 {
     /// <summary>
     /// The different button holding states.
--- devices/Button/GpioButton.cs
+++ devices/Button/GpioButton.cs
@@ -1,4 +1,7 @@
-﻿using System.Device.Gpio;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Device.Gpio;
 
 namespace Iot.Device.Button
 {
