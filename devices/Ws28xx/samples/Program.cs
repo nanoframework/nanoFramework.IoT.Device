f95e5950a12bdeacbd3288ceb1eb6b5ee102d3bb
Robb Schiefer <rschiefer@live.com>
2021-10-12 14:06:02 -0500

    samples project building

--- devices/Ws28xx/samples/Program.cs
+++ devices/Ws28xx/samples/Program.cs
@@ -1,16 +1,15 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using Iot.Device.Ws28xx;
 using System;
-using System.Collections.Generic;
 using System.Device.Spi;
 using System.Drawing;
-using Iot.Device.Graphics;
-using Iot.Device.Ws28xx;
 
 // Configure the count of pixels
 const int Count = 8;
-Console.Clear();
+// TODO - replace or remove
+//Console.Clear();
 
 SpiConnectionSettings settings = new(0, 0)
 {
@@ -26,13 +25,14 @@ Ws28xx neo = new Ws2808(spi, count);
 Ws28xx neo = new Ws2812b(spi, Count);
 #endif
 
-Console.CancelKeyPress += (o, e) =>
-{
-    BitmapImage img = neo.Image;
-    img.Clear();
-    neo.Update();
-    Console.Clear();
-};
+// TODO - refactor once able to test with device
+//Console.CancelKeyPress += (o, e) =>
+//{
+//    BitmapImage img = neo.Image;
+//    img.Clear();
+//    neo.Update();
+//    Console.Clear();
+//};
 
 while (true)
 {
