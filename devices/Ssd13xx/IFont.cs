eaeab1a1d20e735a3a37090fdeda1302244218ba
gligorov <zan@z-softnet.com>
2021-10-13 10:37:28 -0500

    Updated the OLED SSD1306 Demos and Drivers with Basic Functionality t… (#172)

---
+++ devices/Ssd13xx/IFont.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Iot.Device.Ssd13xx
+{
+    /// <summary>
+    /// Base class for font implementation.
+    /// </summary>
+    public abstract class IFont
+    {
+        /// <summary>
+        /// Font width.
+        /// </summary>
+        public virtual byte Width { get; private set; }
+
+        /// <summary>
+        /// Font height.
+        /// </summary>
+        public virtual byte Height { get; private set; }
+
+        /// <summary>
+		///     Get the binary representation of the ASCII character from the font table.
+		/// </summary>
+		/// <param name="character">Character to look up.</param>
+		/// <returns>Array of bytes representing the binary bit pattern of the character.</returns>
+		public abstract byte[] this[char character] { get; }
+    }
+}
