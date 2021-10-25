b6c99b18ec43982ef39870f6fb3a429453ba5cb1
Robb Schiefer <rschiefer@live.com>
2021-10-25 13:49:15 -0500

    Migrated Tcs3472x to nano

---
+++ devices/Tcs3472x/MathExtensions.cs
@@ -0,0 +1,131 @@
+
+using System.Runtime.CompilerServices;
+
+namespace Iot.Device
+{
+    /// <summary>
+    /// Implementations of some functions missing in older .NET versions
+    /// </summary>
+#if BUILDING_IOT_DEVICE_BINDINGS
+    internal
+#else
+    public
+#endif
+    static class MathExtensions
+    {
+        /// <summary>
+        /// Returns val, limited to the range min-max (inclusive)
+        /// </summary>
+        //[MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static double Clamp(double val, double min, double max)
+        {
+#if !NET5_0_OR_GREATER
+            if (val < min)
+            {
+                return min;
+            }
+
+            if (val > max)
+            {
+                return max;
+            }
+
+            return val;
+#else
+            return Math.Clamp(val, min, max);
+#endif
+        }
+
+        /// <summary>
+        /// Returns val, limited to the range min-max (inclusive)
+        /// </summary>
+        //[MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int Clamp(int val, int min, int max)
+        {
+#if !NET5_0_OR_GREATER
+            if (val < min)
+            {
+                return min;
+            }
+
+            if (val > max)
+            {
+                return max;
+            }
+
+            return val;
+#else
+            return Math.Clamp(val, min, max);
+#endif
+        }
+
+        /// <summary>
+        /// Returns val, limited to the range min-max (inclusive)
+        /// </summary>
+        //[MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static byte Clamp(byte val, byte min, byte max)
+        {
+#if !NET5_0_OR_GREATER
+            if (val < min)
+            {
+                return min;
+            }
+
+            if (val > max)
+            {
+                return max;
+            }
+
+            return val;
+#else
+            return Math.Clamp(val, min, max);
+#endif
+        }
+
+        /// <summary>
+        /// Returns val, limited to the range min-max (inclusive)
+        /// </summary>
+        //[MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static long Clamp(long val, long min, long max)
+        {
+#if !NET5_0_OR_GREATER
+            if (val < min)
+            {
+                return min;
+            }
+
+            if (val > max)
+            {
+                return max;
+            }
+
+            return val;
+#else
+            return Math.Clamp(val, min, max);
+#endif
+        }
+
+        /// <summary>
+        /// Returns val, limited to the range min-max (inclusive)
+        /// </summary>
+        //[MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static uint Clamp(uint val, uint min, uint max)
+        {
+#if !NET5_0_OR_GREATER
+            if (val < min)
+            {
+                return min;
+            }
+
+            if (val > max)
+            {
+                return max;
+            }
+
+            return val;
+#else
+            return Math.Clamp(val, min, max);
+#endif
+        }
+    }
+}
