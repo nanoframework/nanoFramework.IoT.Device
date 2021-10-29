63a928460be725fc15ebe569e102347e803ab050
Robb Schiefer <rschiefer@live.com>
2021-10-29 15:36:41 -0500

    updated ina219 to work with nano

--- devices/Ina219/samples/Program.cs
+++ devices/Ina219/samples/Program.cs
@@ -6,12 +6,19 @@ using System.Threading;
 using System.Device.I2c;
 using Iot.Device;
 using Iot.Device.Adc;
+using System.Diagnostics;
+using nanoFramework.Hardware.Esp32;
 
-const byte Adafruit_Ina219_I2cAddress = 0x40;
-const byte Adafruit_Ina219_I2cBus = 0x1;
+const byte Ina219_I2cAddress = 0x40;
+const byte Ina219_I2cBus = 0x1;
+
+
+// Must specify pin functions on ESP32, not needed for most other boards
+Configuration.SetPinFunction(21, DeviceFunction.I2C1_DATA);
+Configuration.SetPinFunction(22, DeviceFunction.I2C1_CLOCK);
 
 // create an INA219 device on I2C bus 1 addressing channel 64
-using Ina219 device = new(new I2cConnectionSettings(Adafruit_Ina219_I2cBus, Adafruit_Ina219_I2cAddress));
+using Ina219 device = new(new I2cConnectionSettings(Ina219_I2cBus, Ina219_I2cAddress));
 // reset the device
 device.Reset();
 
@@ -23,6 +30,6 @@ device.SetCalibration(33574, 12.2e-6f);
 while (true)
 {
     // write out the current values from the INA219 device.
-    Debug.WriteLine($"Bus Voltage {device.ReadBusVoltage()} Shunt Voltage {device.ReadShuntVoltage().Millivolts}mV Current {device.ReadCurrent()} Power {device.ReadPower()}");
+    Debug.WriteLine($"Bus Voltage {device.ReadBusVoltage().Volts} Shunt Voltage {device.ReadShuntVoltage().Millivolts}mV Current {device.ReadCurrent().Value} Power {device.ReadPower().Watts}");
     Thread.Sleep(1000);
 }
