74603a8c94f14ef0cd9583a3af4e3d8051314f9e
José Simões <jose.simoes@eclo.solutions>
2021-11-18 10:50:18 +0000

    Work on sample

--- devices/SwarmTile01/Samples/SwarmTile.Sample/Program.cs
+++ devices/SwarmTile01/Samples/SwarmTile.Sample/Program.cs
@@ -1,37 +1,135 @@
 //
 // Copyright (c) .NET Foundation and Contributors
+// Portions Copyright (c) Eclo Solutions
 // See LICENSE file in the project root for full license information.
 //
 
 using Iot.Device.Swarm;
-using System;
 using System.Diagnostics;
 using System.Threading;
 
+
 Debug.WriteLine("Starting SWARM Tile demo");
 
-var swarmTile = new SwarmTile("COM2");
+SwarmTile swarmTile = new SwarmTile("COM1");
 
 // set this here just for debugging
 swarmTile.TimeoutForCommandExecution = 50_000;
 
-Thread.Sleep(10_000);
+// wait 5 seconds for the Tile to become operational
+if (!swarmTile.DeviceReady.WaitOne(5_000, false))
+{
+    ////////////////////////////
+    // Tile is not responsive //
+    ////////////////////////////
 
-//swarmTile.PowerOff();
+    Debug.WriteLine("****************************************************************");
+    Debug.WriteLine("*** TILE IS NOT RESPONSIVE, POSSIBLY POWERED OFF OR SLEEPING ***");
+    Debug.WriteLine("****************************************************************");
+}
+else
+{
+    // Tile is operational
 
-var rt = swarmTile.GetReceiveTestRate();
+    // output device IDs
+    Debug.WriteLine($"DeviceID: {swarmTile.DeviceID}");
+    Debug.WriteLine($"DeviceName: {swarmTile.DeviceName}");
 
-swarmTile.SetReceiveTestRate(15);
+    // setup event to handle power state change notifications
+    swarmTile.PowerStateChanged += SwarmTile_PowerStateChanged;
 
-//swarmTile.SendToSleep(20);
+    // setup handler for message events
+    swarmTile.MessageEvent += SwarmTile_MessageEvent;
 
-//Message message = new Message("Hello Space! 1805", 12345);
-//message.HoldDuration = 60;
+    // set GPS info rate to each 5 minutes
+    swarmTile.SetGeospatialInformationRate(5 * 60);
 
-//var msgId = swarmTile.TransmitData(message);
+    // set date time information to each minute
+    swarmTile.SetDateTimeStatusRate(60);
+
+    // get count on how many received messages are waiting to be read
+    var unreadCount = swarmTile.MessagesReceived.UnreadCount;
+
+    Debug.WriteLine($"There are {unreadCount} unread messages.");
+
+    if (unreadCount > 0)
+    {
+        bool keepReading = true;
+
+        while (keepReading)
+        {
+            // read newest message
+            var newestMessage = swarmTile.MessagesReceived.ReadNewestMessage();
+
+            if (newestMessage == null)
+            {
+                // no more messages to read
+                keepReading = false;
+            }
+            else
+            {
+                Debug.WriteLine($"+++ New message +++");
+                Debug.WriteLine($"{newestMessage}");
+                Debug.WriteLine("");
+            }
+        }
+    }
+
+    // get count on how many messages are queued for transmission
+    var toTxCount = swarmTile.MessagesToTransmit.Count;
+
+    Debug.WriteLine($"There are {toTxCount} messages queued for transmission.");
+
+    // compose message to transmit
+    MessageToTransmit message = new MessageToTransmit("Hello from .NET nanoFramework!");
+    message.ApplicationID = 12345;
+
+    // send message
+    var msgId = swarmTile.TransmitData(message);
+
+    Debug.WriteLine($"Message {msgId} waiting to be transmitted!");
+}
 
 Thread.Sleep(Timeout.Infinite);
 
-// Browse our samples repository: https://github.com/nanoframework/samples
-// Check our documentation online: https://docs.nanoframework.net/
-// Join our lively Discord community: https://discord.gg/gCyBu8T
+static void SwarmTile_MessageEvent(MessageEvent messageEvent, string messageId)
+{
+    switch (messageEvent)
+    {
+        case MessageEvent.Expired:
+            Debug.WriteLine($"Message {messageId} expired without being transmitted.");
+            break;
+
+        case MessageEvent.Sent:
+            Debug.WriteLine($"Message {messageId} has been successfully transmitted.");
+            break;
+
+        case MessageEvent.Received:
+            Debug.WriteLine($"Just received message {messageId}.");
+            break;
+
+    }
+}
+
+static void SwarmTile_PowerStateChanged(PowerState powerState)
+{
+    switch (powerState)
+    {
+        case PowerState.On:
+            Debug.WriteLine($"Tile is ON.");
+            break;
+
+        case PowerState.Off:
+            Debug.WriteLine($"Tile is OFF.");
+            break;
+
+        case PowerState.Sleep:
+            Debug.WriteLine($"Tile was entered SLEEP.");
+            break;
+
+        case PowerState.Unknown:
+            Debug.WriteLine($"Tile power mode is unknown.");
+            break;
+    }
+}
+
