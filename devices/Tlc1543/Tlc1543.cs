b1c5834928e5ea7338e07d40db41156bf4b9b0b0
Usman Iqbal <usmanatron@hotmail.com>
2022-01-17 17:45:30 +0000

    Migrate Tlc1543 device to nanoFramework (#211)
    * Copy device files (unchanged)
    
    * Migrate code.  Update packages and update README
    
    * Update nuspec and remove unused dependency
    
    * Re-add class comment
    
    * bugfix: Add nullable annotation to remove warnings
    
    * Review Markups
    
    * Add strong names to all package references
    * Add a comment to the Samples regarding setting GPIO pins accordingly
    * Fix file paths for static files in nuspec
    
    * Review Markups
    
    * Remove old version of the code
    
    Co-authored-by: José Simões <jose.simoes@eclo.solutions>

--- devices/Tlc1543/Tlc1543.cs
+++ devices/Tlc1543/Tlc1543.cs
@@ -4,13 +4,12 @@
 using System;
 using System.Device;
 using System.Device.Gpio;
-using System.Collections.Generic;
 using System.Device.Spi;
 
 namespace Iot.Device.Tlc1543
 {
     /// <summary>
-    /// Add documentation here
+    /// Tlc1543 device
     /// </summary>
     public class Tlc1543 : IDisposable
     {
@@ -79,16 +78,9 @@ namespace Iot.Device.Tlc1543
             _spiDevice.TransferFullDuplex(writeBuffer, readBuffer);
 
             int previousReading = ((readBuffer[0] & 0b11111) << 5) | (readBuffer[1] & 0b11111);
-            if (_endOfConversion != -1)
-            {
-                // Wait for ADC to report end of conversion or timeout at max conversion time
-                _controller.WaitForEvent(_endOfConversion, PinEventTypes.Rising, _conversionTime);
-            }
-            else
-            {
-                // Max conversion time (21us) as seen in table on page 10 in TLC1543 documentation
-                DelayHelper.Delay(_conversionTime, false);
-            }
+
+            // Max conversion time (21us) as seen in table on page 10 in TLC1543 documentation
+            DelayHelper.Delay(_conversionTime, false);
 
             return previousReading;
         }
