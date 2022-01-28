db6fccca30414dd8f5ff6b8730441aa5f9cbc12b
Laurent Ellerbach <laurelle@microsoft.com>
2022-01-28 18:34:54 +0300

    Basic project setup

---
+++ devices/Am2320/Am2320.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Iot.Device.Am2320
+{
+    public class Am2320
+    {
+        // Browse our samples repository: https://github.com/nanoframework/samples
+        // Check our documentation online: https://docs.nanoframework.net/
+        // Join our lively Discord community: https://discord.gg/gCyBu8T
+    }
+}
