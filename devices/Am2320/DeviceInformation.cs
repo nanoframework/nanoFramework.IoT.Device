3b4952076c35e61fc97dd70b8321506622932a03
Laurent Ellerbach <laurelle@microsoft.com>
2022-01-30 12:13:54 +0300

    Adding AM2320 (#260)

---
+++ devices/Am2320/DeviceInformation.cs
@@ -0,0 +1,26 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Iot.Device.Am2320
+{
+    /// <summary>
+    /// Device information.
+    /// </summary>
+    public class DeviceInformation
+    {
+        /// <summary>
+        /// The model.
+        /// </summary>
+        public ushort Model { get; set; }
+
+        /// <summary>
+        /// The version.
+        /// </summary>
+        public byte Version { get; set; }
+
+        /// <summary>
+        /// The device ID
+        /// </summary>
+        public uint DeviceId { get; set; }
+    }
+}
