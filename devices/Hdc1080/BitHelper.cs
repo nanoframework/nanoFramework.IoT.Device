819d13730fb7cec27ea80424caec69ce1954eb9c
Rafał Kałwak <rafalekkalwak@wp.pl>
2022-02-09 09:29:30 +0100

    Hdc1080 sensor library (#275)
    * Hdc1080 sensor library
    
    Library for Hdc1080 sensor
    
    Signed-off-by: Rafał Kałwak <rafalekkalwak@wp.pl>
    
    * Hdc1080 Fixed wrong namespace
    
    Fixed wrong namespace
    
    Signed-off-by: Rafał Kałwak <rafalekkalwak@wp.pl>
    
    * Hdc1080 Fixed wrong namespace
    
    Fixed wrong HeatUp function, updated old packages
    
    Signed-off-by: Rafał Kałwak <rafalekkalwak@wp.pl>
    
    * Hdc1080 Fixed wrong nuspec
    
    Updated old packages
    
    Signed-off-by: Rafał Kałwak <rafalekkalwak@wp.pl>
    
    * Hdc1080 Fixed wrong nuspec
    
    Updated old packages
    
    Signed-off-by: Rafał Kałwak <rafalekkalwak@wp.pl>
    
    * Hdc1080 Refactored to use properties instead of methods
    
    Refactored to use properties instead of methods
    
    Signed-off-by: Rafał Kałwak <rafalekkalwak@wp.pl>
    
    * Hdc1080 Refactored to use proper spacing
    
    Refactored to use proper spacing
    
    Signed-off-by: Rafał Kałwak <rafalekkalwak@wp.pl>
    
    Co-authored-by: Rafał <Kałwak>
    Co-authored-by: Laurent Ellerbach <laurelle@microsoft.com>

---
+++ devices/Hdc1080/BitHelper.cs
@@ -0,0 +1,25 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Iot.Device.Hdc1080
+{
+    /// <summary>
+    /// Helper that allows setting and getting each bit separately
+    /// </summary>
+    internal static class BitHelper
+    {
+        public static byte SetBit(this byte b, int pos, bool value)
+        {
+            if (value)
+            {
+                return (byte)(b | (1 << pos));
+            }
+            return (byte)(b & ~(1 << pos));
+        }
+
+        public static bool GetBit(this byte b, int bitNumber)
+        {
+            return (b & (1 << bitNumber - 1)) != 0;
+        }
+    }
+}
