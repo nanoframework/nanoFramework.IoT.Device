c6ad9b62fc3ad0131b4b1fa763dbc2a0fe427e49
Robin Jones <robin@networkfusion.co.uk>
2022-03-10 21:14:28 +0000

    Added headers for `SpanChar` and `SpanPinValuePair` classes
    (even though they are taken from autogenerated classes).
    Add System.Drawing nuget for future need of color.
    Remove custom color class.

--- devices/CharacterLcd/custom/ImageSharp.cs
+++ devices/CharacterLcd/custom/ImageSharp.cs
@@ -34,38 +34,4 @@ namespace SixLabors.ImageSharp
             Height = height;
         }
     }
-
-    ///// <content>
-    ///// Contains static named color values.
-    ///// <see href="https://www.w3.org/TR/css-color-3/"/>
-    ///// </content>
-    //public class Color
-    //{
-    //    /// <summary>
-    //    /// Represents a <see paramref="Color"/> matching the W3C definition that has an hex value of #000000.
-    //    /// </summary>
-    //    public static readonly Color Black = FromRgba(0, 0, 0, 255);
-
-
-    //    /// <summary>
-    //    /// Represents a <see paramref="Color"/> matching the W3C definition that has an hex value of #FFFFFF.
-    //    /// </summary>
-    //    public static readonly Color White = FromRgba(255, 255, 255, 255);
-
-    //    /// <summary>
-    //    /// Creates a <see cref="Color"/> from RGBA bytes.
-    //    /// </summary>
-    //    /// <param name="r">The red component (0-255).</param>
-    //    /// <param name="g">The green component (0-255).</param>
-    //    /// <param name="b">The blue component (0-255).</param>
-    //    /// <param name="a">The alpha component (0-255).</param>
-    //    /// <returns>The <see cref="Color"/>.</returns>
-    //    public static Color FromRgba(byte r, byte g, byte b, byte a) => new(r, g, b, a);
-
-    //    public static int ToPixel(int color)
-    //    {
-    //        return color;
-    //    }
-
-    //}
 }
