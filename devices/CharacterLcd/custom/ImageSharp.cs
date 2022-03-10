77f76d672047532f3fd3a08678e9a4ef799ff484
Robin Jones <robin@networkfusion.co.uk>
2022-03-10 18:25:38 +0000

    Further documentation improvements.

--- devices/CharacterLcd/custom/ImageSharp.cs
+++ devices/CharacterLcd/custom/ImageSharp.cs
@@ -13,18 +13,33 @@ namespace SixLabors.ImageSharp
     /// </remarks>
     public struct Size
     {
+        /// <summary>
+        /// Gets or sets the height of this <see cref="Size"/>.
+        /// </summary>
         public int Height { get; set; }
+
+        /// <summary>
+        /// Gets or sets the width of this <see cref="Size"/>.
+        /// </summary>
         public int Width { get; set; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Size"/> struct.
+        /// </summary>
+        /// <param name="width">The width of the size.</param>
+        /// <param name="height">The height of the size.</param>
         public Size(int width, int height)
         {
             Width = width;
             Height = height;
         }
-
     }
 
-    //public class PixelFormats //.Color
+    ///// <content>
+    ///// Contains static named color values.
+    ///// <see href="https://www.w3.org/TR/css-color-3/"/>
+    ///// </content>
+    //public class Color
     //{
     //    /// <summary>
     //    /// Represents a <see paramref="Color"/> matching the W3C definition that has an hex value of #000000.
@@ -36,6 +51,16 @@ namespace SixLabors.ImageSharp
     //    /// Represents a <see paramref="Color"/> matching the W3C definition that has an hex value of #FFFFFF.
     //    /// </summary>
     //    public static readonly Color White = FromRgba(255, 255, 255, 255);
+
+    //    /// <summary>
+    //    /// Creates a <see cref="Color"/> from RGBA bytes.
+    //    /// </summary>
+    //    /// <param name="r">The red component (0-255).</param>
+    //    /// <param name="g">The green component (0-255).</param>
+    //    /// <param name="b">The blue component (0-255).</param>
+    //    /// <param name="a">The alpha component (0-255).</param>
+    //    /// <returns>The <see cref="Color"/>.</returns>
+    //    public static Color FromRgba(byte r, byte g, byte b, byte a) => new(r, g, b, a);
 
     //    public static int ToPixel(int color)
     //    {
