ad3dbf6a40c0413527ac30aa85f37b5a34b8833e
Robin Jones <robin@networkfusion.co.uk>
2022-03-15 01:27:48 +0000

    First itteration.
    This code (should allow) the same functionality as the `PCF8563`.
    The extra functionality is currently commented out.

---
+++ devices/Rtc/Devices/Pcf85263/BitHelper.cs
@@ -0,0 +1,20 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Iot.Device.Rtc
+{
+    /// <summary>
+    /// Helper that allows setting and getting each bit separately
+    /// </summary>
+    internal static class BitHelper
+    {
+        public static byte ClearBit(this byte data, int bitNumber) => (byte)(data & ~(1 << bitNumber));
+
+        public static bool IsBitSet(this byte data, int bitNumber) => (data & (1 << bitNumber)) != 0;
+
+        public static byte SetBit(byte data, int bitNumber) => (byte)(data | (1 << bitNumber));
+
+        public static bool GetBit(this byte b, int bitNumber) =>  (b & (1 << bitNumber - 1)) != 0;
+
+    }
+}
