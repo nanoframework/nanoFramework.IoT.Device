d8bb7d80cf4d29dda900533fd4852636de18a47c
José Simões <jose.simoes@eclo.solutions>
2022-05-23 10:54:54 +0100

    Fix from code review

--- devices/Hx711/Properties/AssemblyInfo.cs
+++ devices/Hx711/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-
 [assembly: AssemblyTitle("Iot.Device.Hx711")]
 [assembly: AssemblyCompany(".NET nanoFramework Contributors")]
 [assembly: AssemblyCopyright("Copyright(c).NET Foundation and Contributors")]
