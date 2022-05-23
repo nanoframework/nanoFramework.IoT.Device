27ea7b2ce1297e8d132761f1345210357df25444
José Simões <jose.simoes@eclo.solutions>
2022-05-23 12:17:24 +0100

    Add Hx711 from M5Stack WEIGHT module (#399)

---
+++ devices/Hx711/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Iot.Device.Hx711")]
+[assembly: AssemblyCompany(".NET nanoFramework Contributors")]
+[assembly: AssemblyCopyright("Copyright(c).NET Foundation and Contributors")]
+
+[assembly: ComVisible(false)]
