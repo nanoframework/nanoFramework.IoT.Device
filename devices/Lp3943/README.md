2a69d87b12af31569ff034ce9cdf9e530e884f77
Kasper de Vries <89792927+KasperJSdeVries@users.noreply.github.com>
2022-06-01 17:07:47 +0200

    Implement the Driver

--- devices/Lp3943/README.md
+++ devices/Lp3943/README.md
@@ -1,9 +1,11 @@
 # Lp3943 LED driver
 
 The LP3943 is an integrated device capable of independently driving 16 LEDs. It uses I2C.
+
 ## Documentation
 
 - LM75 [datasheet](https://www.ti.com/lit/ds/symlink/lp3943.pdf)
 
 ## Usage
+
 placeholder text
--- devices/Lp3943/samples/Program.cs
+++ devices/Lp3943/samples/Program.cs
@@ -1,12 +1,19 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Diagnostics;
+using System.Device.I2c;
 using System.Threading;
 using Iot.Device.Lp3943;
 
-Debug.WriteLine("Hello");
+I2cConnectionSettings settings = new(3, Lp3943.DefaultI2cAddress);
+using var device = new I2cDevice(settings);
 
-var lp3943 = new Lp3943();
+using var ledDriver = new Lp3943(device);
+
+ledDriver.DimRegister(DimRegister.Dim0, 1, 25);
+ledDriver.DimRegister(DimRegister.Dim1, 5, 50);
+ledDriver.SetLed(13, LedState.Off);
+ledDriver.SetLed(14, LedState.Off);
+ledDriver.SetLed(15, LedState.Off);
 
 Thread.Sleep(Timeout.Infinite);
