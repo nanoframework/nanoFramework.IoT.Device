b932e9edcc29dd4cf7d60758877bee77405ef556
Kasper de Vries <89792927+KasperJSdeVries@users.noreply.github.com>
2022-06-02 10:03:40 +0200

    Update the sample to use all public features

--- devices/Lp3943/samples/Program.cs
+++ devices/Lp3943/samples/Program.cs
@@ -12,8 +12,11 @@ using var ledDriver = new Lp3943(device);
 
 ledDriver.DimRegister(DimRegister.Dim0, 1, 25);
 ledDriver.DimRegister(DimRegister.Dim1, 5, 50);
-ledDriver.SetLed(13, LedState.Off);
-ledDriver.SetLed(14, LedState.Off);
-ledDriver.SetLed(15, LedState.Off);
+ledDriver.SetLed(new [] { 12, 13, 14, 15 }, LedState.Off);
+ledDriver.SetLed(new[] { 0, 1, 2, 3, 4, 5 }, LedState.On);
+ledDriver.SetLed(new[] { 6, 7, 8 }, LedState.Dim0);
+ledDriver.SetLed(9, LedState.Dim1);
+ledDriver.SetLed(10, LedState.Dim1);
+ledDriver.SetLed(11, LedState.Dim1);
 
 Thread.Sleep(Timeout.Infinite);
