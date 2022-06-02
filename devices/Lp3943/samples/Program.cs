e950d3c0145d482a3349e7b74a5df5287cbea234
Kasper de Vries <89792927+KasperJSdeVries@users.noreply.github.com>
2022-06-02 17:47:26 +0200

    Program.cs: Fix issues raised in review

--- devices/Lp3943/samples/Program.cs
+++ devices/Lp3943/samples/Program.cs
@@ -6,14 +6,28 @@ using System.Threading;
 using Iot.Device.Lp3943;
 using Iot.Device.Lp3943.Samples;
 
+//////////////////////////////////////////////////////////////////////
+// when connecting to an ESP32 device, need to configure the I2C GPIOs
+// used for the bus
+// Configuration.SetPinFunction(21, DeviceFunction.I2C1_DATA);
+// Configuration.SetPinFunction(22, DeviceFunction.I2C1_CLOCK);
+
 I2cConnectionSettings settings = new(3, Lp3943.DefaultI2cAddress);
 using var device = new I2cDevice(settings);
 var resetPin = Utilities.GetPinNumber('D', 10);
+///////////////////////////////////////////////////////////////////////
+// When using an ESP32 device:
+// var resetPin = 32;
 
 using var ledDriver = new Lp3943(device, resetPin);
 
+// Dim LEDs 0 to 7 at 1 Hz at 25% duty cycle
 ledDriver.DimRegister(DimRegister.Dim0, 1, 25);
+
+// Dim LEDs 8 to 12 at 5 Hz at 50% duty cycle
 ledDriver.DimRegister(DimRegister.Dim1, 5, 50);
+
+// Set LEDs 12, 13, 14, 15 off
 ledDriver.SetLed(
 	new[]
 	{
@@ -24,6 +38,8 @@ ledDriver.SetLed(
 	},
 	LedState.Off
 );
+
+// Set LEDs 0, 1, 2, 3, 4, 5 on
 ledDriver.SetLed(
 	new[]
 	{
@@ -36,6 +52,8 @@ ledDriver.SetLed(
 	},
 	LedState.On
 );
+
+// Set LEDs 6, 7, 8 to be powered by Dim register 0
 ledDriver.SetLed(
 	new[]
 	{
@@ -45,6 +63,8 @@ ledDriver.SetLed(
 	},
 	LedState.Dim0
 );
+
+// Set LEDs 9, 10, 11 to be powered by Dim register 1
 ledDriver.SetLed(9, LedState.Dim1);
 ledDriver.SetLed(10, LedState.Dim1);
 ledDriver.SetLed(11, LedState.Dim1);
