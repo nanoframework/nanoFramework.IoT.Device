b2fb88157d9e06369760c5073153f3abe63d3a06
Michael R Wheeley <MichaelWheeley@users.noreply.github.com>
2022-06-07 06:57:07 -0700

    LCD library for SparkFun RGB Serial Open LCD display (sizes 20x4 or 16x2) with I2C connection (#423)

---
+++ devices/SparkFunLcd/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Iot.Device.SparkFunLcd")]
+[assembly: AssemblyCompany("nanoFramework Contributors")]
+[assembly: AssemblyCopyright("Copyright(c).NET Foundation and Contributors")]
+
+[assembly: ComVisible(false)]
