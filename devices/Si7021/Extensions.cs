30ae8fb31aa4c4b81f064b3f315c0813e3bece99
José Simões <jose.simoes@eclo.solutions>
2022-06-15 01:11:07 +0100

    Improvements in Si7021
    - Add new commands to read device serial.
    - Improve code to parse fw revision to use an enum and show better output.
    - Add extensions to output fw revision and serial ID as string.
    - Rename Register enum to Command as these are I2C commands and not registers.
    - Remove sleep from all commands not requiring it.
    - Replace commands to read temp and RH with non-holding versions (not needed as we're pausing enough time to allow a conversion).
    - Minor improvements in Intellisense comments and formating.
    - Update format string in sample and readme (0.# is not supported in .NET nanoFramework).
    - Update nbgv nuget.
    - Add files to solution for quick access from VS.
    - Bump version to 1.2

---
+++ devices/Si7021/Extensions.cs
@@ -0,0 +1,48 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Iot.Device.Si7021
+{
+    /// <summary>
+    /// Extensions for <see cref="Si7021"/> class.
+    /// </summary>
+    static public class Extensions
+    {
+        /// <summary>
+        /// Returns a string representation of <see cref="FirmwareRevision"/>.
+        /// </summary>
+        /// <param name="value">The <see cref="FirmwareRevision"/> to convert to a <see cref="string"/>.</param>
+        /// <returns>A <see cref="string"/> with the representation of <paramref name="value"/>.</returns>
+        public static string AsText(this FirmwareRevision value)
+        {
+            switch (value)
+            {
+                case FirmwareRevision.V2_0:
+                    return "2.0";
+
+                case FirmwareRevision.V1_0:
+                    return "2.0";
+
+                default:
+                    return "?.?";
+            }
+        }
+
+        /// <summary>
+        /// Returns a string representation of a byte array. Hexadecimal formated.
+        /// </summary>
+        /// <param name="array">The <see cref="byte"/>[] to convert to a <see cref="string"/>.</param>
+        /// <returns>A <see cref="string"/> with the representation of <paramref name="array"/>.</returns>
+        public static string AsText(this byte[] array)
+        {
+            string output = "";
+
+            foreach(var item in array)
+            {
+                output += item.ToString("X2");
+            }
+
+            return output;
+        }
+    }
+}
