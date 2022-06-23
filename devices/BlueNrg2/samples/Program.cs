ac8cae699c51cca8b7060978a84222629391867a
Kasper de Vries <89792927+KasperJSdeVries@users.noreply.github.com>
2022-06-23 14:42:29 +0200

    Adds licencing comments

--- devices/BlueNrg2/samples/Program.cs
+++ devices/BlueNrg2/samples/Program.cs
@@ -1,3 +1,6 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
 using System.Device.Gpio;
 using System.Device.Spi;
 using System.Diagnostics;
