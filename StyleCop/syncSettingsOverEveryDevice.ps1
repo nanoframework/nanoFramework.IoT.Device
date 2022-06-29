b54130d019e93858cdbc8092716e0fb219f82fe9
Marcin Torba <torbamarcin@gmail.com>
2022-06-29 21:10:02 +0200

    Create script for sync settings over all devices sources

---
+++ StyleCop/syncSettingsOverEveryDevice.ps1
@@ -0,0 +1,37 @@
+$styleCopSettingsFileName = "Settings.StyleCop";
+$devicesMainFolderPath = "..\devices"
+$sourceFileContent = Get-Content $styleCopSettingsFileName
+
+$allDevicesFolders = Get-ChildItem -Path $devicesMainFolderPath -Directory
+
+function Test-PSCustomObjectEquality {
+  param(
+    [Parameter(Mandatory = $true)]
+    [PSCustomObject] $firstObject,
+
+    [Parameter(Mandatory = $true)]
+    [PSCustomObject] $secondObject
+  )
+  -not (Compare-Object $firstObject.PSObject.Properties $secondObject.PSObject.Properties)
+}
+
+foreach ($deviceFolder in $allDevicesFolders) {
+
+   $pathToSettingInCurrentFolder = $deviceFolder.FullName + '\' + $styleCopSettingsFileName
+
+   if (Test-Path -Path $pathToSettingInCurrentFolder -PathType Leaf) 
+   {
+      Write-Host "File exists in path " $pathToSettingInCurrentFolder". Checking file content" -ForegroundColor Green
+      $contentOfExistingFile = Get-Content $pathToSettingInCurrentFolder
+
+      $comapreResult = (Compare-Object $sourceFileContent $contentOfExistingFile)
+      if ($comapreResult.Count -eq 0)
+      {
+        Write-Host "Existing file matches source" -ForegroundColor Green
+        continue
+      }
+   }
+
+   Write-Host "Creating/replacing file in " $pathToSettingInCurrentFolder -ForegroundColor Yellow
+   Set-Content $pathToSettingInCurrentFolder $sourceFileContent
+ }
