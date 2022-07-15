14e84dfd18cd88cdd4934842d9de8b807afa3cf4
Laurent Ellerbach <laurelle@microsoft.com>
2022-07-15 15:37:00 +0200

    Adjusting StyleCope rules, fixing all issues, adjusting project placement

--- devices/SwarmTile/SwarmTile/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Iot.Device.SwarmTile")]
-[assembly: AssemblyCompany("nanoFramework Contributors")]
-[assembly: AssemblyCopyright("Copyright(c).NET Foundation and Contributors")]
-
-[assembly: ComVisible(false)]
