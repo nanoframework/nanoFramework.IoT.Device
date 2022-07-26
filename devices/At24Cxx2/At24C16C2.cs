7a8e951dc88cda603234ca34e9a29aea03b9d0b0
DoNotTapeOver <58527684+DoNotTapeOver@users.noreply.github.com>
2022-07-26 15:10:02 -0700

    Naming convention change stage 1

---
+++ devices/At24Cxx2/At24C16C2.cs
@@ -0,0 +1,22 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Device.I2c;
+
+namespace Iot.Device.At24Cxx
+{
+    /// <summary>
+    /// I2C EEPROM with 16Kbit (2048 bytes) of memory internally organized as 128 pages containing 16 bytes each.
+    /// </summary>
+    public sealed class At24C16C : At24Base
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="At24C16C" /> class.
+        /// </summary>
+        /// <param name="i2cDevice">The I2C device to use for communication.</param>
+        public At24C16C(I2cDevice i2cDevice)
+            : base(i2cDevice, 16, 128)
+        {
+        }
+    }
+}
---
+++ devices/At24Cxx2/At24C256C2.cs
@@ -0,0 +1,22 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Device.I2c;
+
+namespace Iot.Device.At24Cxx
+{
+    /// <summary>
+    /// I2C EEPROM with 256Kbit (32768 bytes) of memory internally organized as 512 pages containing 64 bytes each.
+    /// </summary>
+    public sealed class At24C256C : At24Base
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="At24C256C" /> class.
+        /// </summary>
+        /// <param name="i2cDevice">The I2C device to use for communication.</param>
+        public At24C256C(I2cDevice i2cDevice)
+            : base(i2cDevice, 64, 512)
+        {
+        }
+    }
+}
