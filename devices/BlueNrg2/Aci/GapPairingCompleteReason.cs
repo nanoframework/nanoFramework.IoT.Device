8d5fef387fc37380463f892f700aa314a31982a4
Kasper de Vries <89792927+KasperJSdeVries@users.noreply.github.com>
2022-08-01 16:10:02 +0200

    Adds Copyright headers

--- devices/BlueNrg2/Aci/GapPairingCompleteReason.cs
+++ devices/BlueNrg2/Aci/GapPairingCompleteReason.cs
@@ -1,4 +1,7 @@
-﻿using Iot.Device.BlueNrg2.Aci.Events;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Iot.Device.BlueNrg2.Aci.Events;
 
 namespace Iot.Device.BlueNrg2.Aci
 {
--- devices/BlueNrg2/Aci/KeyPressNotificationType.cs
+++ devices/BlueNrg2/Aci/KeyPressNotificationType.cs
@@ -1,11 +1,36 @@
-﻿namespace Iot.Device.BlueNrg2.Aci
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Iot.Device.BlueNrg2.Aci
 {
+    /// <summary>
+    /// Type of keypress the notification reports.
+    /// </summary>
     public enum KeyPressNotificationType
     {
+        /// <summary>
+        /// Entry started.
+        /// </summary>
         EntryStarted = 0x00,
+
+        /// <summary>
+        /// Digit entered.
+        /// </summary>
         DigitEntered = 0x01,
+
+        /// <summary>
+        /// Digit erased.
+        /// </summary>
         DigitErased = 0x02,
+
+        /// <summary>
+        /// Cleared.
+        /// </summary>
         Cleared = 0x03,
+
+        /// <summary>
+        /// Entry Completed.
+        /// </summary>
         EntryCompleted = 0x04
     }
 }
