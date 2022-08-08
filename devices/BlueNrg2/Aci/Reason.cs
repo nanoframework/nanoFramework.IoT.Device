a8a4e5a33911dafd00b832965f08567507fff650
Kasper de Vries <89792927+KasperJSdeVries@users.noreply.github.com>
2022-08-08 13:44:19 +0200

    Adds copyright headers to ACI.

--- devices/BlueNrg2/Aci/Reason.cs
+++ devices/BlueNrg2/Aci/Reason.cs
@@ -1,4 +1,7 @@
-﻿namespace Iot.Device.BlueNrg2.Aci
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Iot.Device.BlueNrg2.Aci
 {
     /// <summary>
     /// Reason for behaviour.
