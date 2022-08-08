2397876d413078b2e9d4a649eeb6c7246909a551
Kasper de Vries <89792927+KasperJSdeVries@users.noreply.github.com>
2022-08-08 16:44:31 +0200

    Update sample to something that should do something interesting.

--- devices/BlueNrg2/samples/Program.cs
+++ devices/BlueNrg2/samples/Program.cs
@@ -1,10 +1,13 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
 using System.Device.Gpio;
 using System.Device.Spi;
 using System.Diagnostics;
+using System.Text;
 using System.Threading;
+using Iot.Device.BlueNrg2.Aci;
 
 namespace Iot.Device.BlueNrg2.Samples
 {
@@ -31,6 +34,56 @@ namespace Iot.Device.BlueNrg2.Samples
             blueNrg2.StartBluetoothThread();
 
             blueNrg2.Gatt.Init();
+
+            var deviceName = "NanoFramework bluetooth test!";
+            ushort deviceNameServiceHandle;
+            ushort deviceNameCharacteristicHandle;
+            ushort appearanceCharacteristicHandle;
+
+            blueNrg2.Gap.Init(
+                Role.Central,
+                false,
+                (byte)deviceName.Length,
+                out deviceNameServiceHandle,
+                out deviceNameCharacteristicHandle,
+                out appearanceCharacteristicHandle
+            );
+
+            blueNrg2.Gatt.UpdateCharacteristicValue(
+                deviceNameServiceHandle,
+                deviceNameCharacteristicHandle,
+                0,
+                (byte)deviceName.Length,
+                Encoding.UTF8.GetBytes(deviceName)
+            );
+
+            Guid serviceUuid;
+            Guid characteristicUuid;
+            Guid.TryParseGuidWithDashes("f162b0d0-1715-11ed-861d-0242ac120002", out serviceUuid);
+            Guid.TryParseGuidWithDashes("b9702b10-1717-11ed-861d-0242ac120002", out characteristicUuid);
+
+            ushort serviceHandle;
+            ushort characteristicHandle;
+            if (blueNrg2.Gatt.AddService(UuidType.Uuid128, serviceUuid.ToByteArray(), ServiceType.Primary, 1, out serviceHandle) != BleStatus.Success)
+                return;
+
+            if (blueNrg2.Gatt.AddCharacteristic(
+                    serviceHandle,
+                    UuidType.Uuid128,
+                    characteristicUuid.ToByteArray(),
+                    1,
+                    CharacteristicProperties.Read | CharacteristicProperties.Write,
+                    SecurityPermissions.None,
+                    CharacteristicEventMask.NotifyReadRequestAndWaitForApprovalResponse |
+                    CharacteristicEventMask.NotifyWriteRequestAndWaitForApprovalResponse,
+                    0,
+                    true,
+                    out characteristicHandle
+                ) != BleStatus.Success)
+                return;
+
+            blueNrg2.Gatt.UpdateCharacteristicValue(serviceHandle, characteristicHandle, 0, 0, Encoding.UTF8.GetBytes("Hello World from BlueNRG-2"));
+            blueNrg2.Hci.LeSetAdvertiseEnable(true);
 
             while (true)
             {
