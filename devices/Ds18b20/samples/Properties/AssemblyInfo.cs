8c5663ca36a8b96c77ff82875b372f21f71329a4
Rafał Kałwak <rafalekkalwak@wp.pl>
2022-08-16 11:13:19 +0200

    Ds18b20 - temperature sensor library (#526)
    * Initial version
    
    Signed-off-by: Rafał Kałwak <rafalekkalwak@wp.pl>
    
    * Added dispose
    
    * running version
    
    * running version
    
    * running version
    
    * Stylecop
    
    * docs
    
    * sample
    
    * libs
    
    * assembly name
    
    * assembly name
    
    * assembly name
    
    * assembly name
    
    * assembly name and namespace fix
    
    * PR comments
    
    * PR comments resolving
    
    * added TryGetValue pattern
    
    * readme update
    
    Signed-off-by: Rafał Kałwak <rafalekkalwak@wp.pl>
    Co-authored-by: Rafał <Kałwak>
    Co-authored-by: Laurent Ellerbach <laurelle@microsoft.com>

---
+++ devices/Ds18b20/samples/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Iot.Device.Ds18b20.Samples")]
+[assembly: AssemblyCompany("nanoFramework Contributors")]
+[assembly: AssemblyCopyright("Copyright(c).NET Foundation and Contributors")]
+
+[assembly: ComVisible(false)]
