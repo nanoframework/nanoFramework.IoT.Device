1e88d2a3627b90dac3962343c5610ab32f5c951a
Mr CSharp <rafael.yousuf@outlook.com>
2022-09-09 19:51:52 +1000

    Port pcd8544 bindings from .Net IoT (#557)
    * port the pcd8544 library to nanoframework (no samples)
    
    * feat: port the samples codes (no longer a console app)
    
    * fix: argument exception when position is outside of display buffer range
    
    * feat: adjust namespace and fix xml docs export
    
    * feat: add readme file
    
    * chore: add copyright notice on new/original files
    
    * chore: move img2pcd8544 project into the pcd8544 folder
    
    * chore: resolve PR comments
    
    * chore: apply code style fixes from StyleCop warnings
    
    * chore: fix markdown code sample
    
    * Update devices/Pcd8544/README.md
    
    Co-authored-by: Laurent Ellerbach <laurelle@microsoft.com>
    
    Co-authored-by: Laurent Ellerbach <laurelle@microsoft.com>

---
+++ devices/Pcd8544/Img2Pcd8544/Program.cs
@@ -0,0 +1,33 @@
+﻿//
+// Copyright (c) 2022 The nanoFramework project contributors
+// See LICENSE file in the project root for full license information.
+//
+
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+
+var imageFilePath = args[0];
+using var image = Image.Load<Rgba32>(imageFilePath);
+image.Mutate(x => x.Resize(new Size(84, 48)));
+image.Mutate(x => x.BlackWhite());
+
+var colWhite = new Rgba32(255, 255, 255);
+var width = 84;
+var result = new byte[504];
+for (var pos = 0; pos < result.Length; pos++)
+{
+    byte toStore = 0;
+    for (int bit = 0; bit < 8; bit++)
+    {
+        var x = pos % width;
+        var y = pos / width * 8 + bit;
+        toStore = (byte)(toStore | ((image[x, y] == colWhite ? 0 : 1) << bit));
+    }
+
+    result[pos] = toStore;
+}
+
+var resultString = $"var bitmap = new byte[] {{{String.Join(",", result.Select(b => $"0x{b.ToString("X2")}"))}}}";
+Console.WriteLine(resultString);
+Console.ReadKey();
---
+++ devices/Pcd8544/samples/Pcd8544.samples/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Pcd8544.samples")]
+[assembly: AssemblyCompany("nanoFramework Contributors")]
+[assembly: AssemblyCopyright("Copyright(c).NET Foundation and Contributors")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
