d9be11026d7e435fe3e8eb11a9f5d0ee0722f39b
Marcin Torba <torbamarcin@gmail.com>
2022-09-30 00:07:30 +0200

    BH1750FVI style cop.

--- devices/Bh1750fvi/I2cAddress.cs
+++ devices/Bh1750fvi/I2cAddress.cs
@@ -4,17 +4,17 @@
 namespace Iot.Device.Bh1750fvi
 {
     /// <summary>
-    /// BH1750FVI I2C Address
+    /// BH1750FVI I2C Address.
     /// </summary>
     public enum I2cAddress : byte
     {
         /// <summary>
-        /// ADD Pin connect to high power level
+        /// ADD Pin connect to high power level.
         /// </summary>
         AddPinHigh = 0x5C,
 
         /// <summary>
-        /// ADD Pin connect to low power level
+        /// ADD Pin connect to low power level.
         /// </summary>
         AddPinLow = 0x23
     }
