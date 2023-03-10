4c5761d6d9b1196a5d6f6385f27b8c3546605a9a
jacquesoberto <127383259+jacquesoberto@users.noreply.github.com>
2023-03-10 21:48:33 +0100

    Add files via upload

---
+++ devices/Ssd13xx/Bitmap2Font/Bitmap2Font.Designer.cs
@@ -0,0 +1,388 @@
+﻿namespace Bitmap2Font
+{
+    partial class Bitmap2Font
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.OpenButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.Font_Width_Sel = new System.Windows.Forms.NumericUpDown();
+            this.Font_Height_Sel = new System.Windows.Forms.NumericUpDown();
+            this.Bmp_End_Sel = new System.Windows.Forms.NumericUpDown();
+            this.Bmp_Col_Sel = new System.Windows.Forms.NumericUpDown();
+            this.Bmp_Start_Sel = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.PrevButton = new System.Windows.Forms.Button();
+            this.GenButton = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Font_Width_Sel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Font_Height_Sel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Bmp_End_Sel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Bmp_Col_Sel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Bmp_Start_Sel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(32, 86);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(449, 460);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(648, 86);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(449, 460);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // OpenButton
+            // 
+            this.OpenButton.Location = new System.Drawing.Point(32, 20);
+            this.OpenButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.OpenButton.Name = "OpenButton";
+            this.OpenButton.Size = new System.Drawing.Size(200, 35);
+            this.OpenButton.TabIndex = 2;
+            this.OpenButton.Text = "Open Bitmap";
+            this.OpenButton.UseVisualStyleBackColor = true;
+            this.OpenButton.Click += new System.EventHandler(this.OpenButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(32, 578);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(448, 107);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Font_Width_Sel
+            // 
+            this.Font_Width_Sel.Location = new System.Drawing.Point(650, 577);
+            this.Font_Width_Sel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Font_Width_Sel.Maximum = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            this.Font_Width_Sel.Minimum = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.Font_Width_Sel.Name = "Font_Width_Sel";
+            this.Font_Width_Sel.Size = new System.Drawing.Size(68, 26);
+            this.Font_Width_Sel.TabIndex = 6;
+            this.Font_Width_Sel.Value = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            // 
+            // Font_Height_Sel
+            // 
+            this.Font_Height_Sel.Location = new System.Drawing.Point(650, 617);
+            this.Font_Height_Sel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Font_Height_Sel.Maximum = new decimal(new int[] {
+            40,
+            0,
+            0,
+            0});
+            this.Font_Height_Sel.Minimum = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.Font_Height_Sel.Name = "Font_Height_Sel";
+            this.Font_Height_Sel.Size = new System.Drawing.Size(68, 26);
+            this.Font_Height_Sel.TabIndex = 7;
+            this.Font_Height_Sel.Value = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            // 
+            // Bmp_End_Sel
+            // 
+            this.Bmp_End_Sel.Location = new System.Drawing.Point(873, 657);
+            this.Bmp_End_Sel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Bmp_End_Sel.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.Bmp_End_Sel.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.Bmp_End_Sel.Name = "Bmp_End_Sel";
+            this.Bmp_End_Sel.Size = new System.Drawing.Size(68, 26);
+            this.Bmp_End_Sel.TabIndex = 8;
+            this.Bmp_End_Sel.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            // 
+            // Bmp_Col_Sel
+            // 
+            this.Bmp_Col_Sel.Location = new System.Drawing.Point(874, 577);
+            this.Bmp_Col_Sel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Bmp_Col_Sel.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.Bmp_Col_Sel.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.Bmp_Col_Sel.Name = "Bmp_Col_Sel";
+            this.Bmp_Col_Sel.Size = new System.Drawing.Size(68, 26);
+            this.Bmp_Col_Sel.TabIndex = 9;
+            this.Bmp_Col_Sel.Value = new decimal(new int[] {
+            32,
+            0,
+            0,
+            0});
+            // 
+            // Bmp_Start_Sel
+            // 
+            this.Bmp_Start_Sel.Location = new System.Drawing.Point(874, 617);
+            this.Bmp_Start_Sel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Bmp_Start_Sel.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.Bmp_Start_Sel.Name = "Bmp_Start_Sel";
+            this.Bmp_Start_Sel.Size = new System.Drawing.Size(68, 26);
+            this.Bmp_Start_Sel.TabIndex = 10;
+            this.Bmp_Start_Sel.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(722, 580);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 20);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Font Width (px)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(722, 620);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 20);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Font Height (px)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(946, 580);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 20);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Bitmap Columns";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(946, 620);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(210, 20);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Bitmap Starting Row (top=0)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(946, 660);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(137, 20);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "<Bitmap End Row";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(240, 23);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(856, 26);
+            this.textBox2.TabIndex = 16;
+            // 
+            // PrevButton
+            // 
+            this.PrevButton.Location = new System.Drawing.Point(512, 108);
+            this.PrevButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.PrevButton.Name = "PrevButton";
+            this.PrevButton.Size = new System.Drawing.Size(112, 35);
+            this.PrevButton.TabIndex = 18;
+            this.PrevButton.Text = "Preview";
+            this.PrevButton.UseVisualStyleBackColor = true;
+            this.PrevButton.Click += new System.EventHandler(this.PrevButton_Click);
+            // 
+            // GenButton
+            // 
+            this.GenButton.Location = new System.Drawing.Point(512, 180);
+            this.GenButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.GenButton.Name = "GenButton";
+            this.GenButton.Size = new System.Drawing.Size(112, 35);
+            this.GenButton.TabIndex = 19;
+            this.GenButton.Text = "Generate";
+            this.GenButton.UseVisualStyleBackColor = true;
+            this.GenButton.Click += new System.EventHandler(this.GenButton_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(36, 63);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(116, 20);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Original Bitmap";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(656, 63);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(117, 20);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "Preview Bitmap";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(36, 554);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(148, 20);
+            this.label8.TabIndex = 22;
+            this.label8.Text = "Original Bitmap Info";
+            // 
+            // Bitmap2Font
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1170, 714);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.GenButton);
+            this.Controls.Add(this.PrevButton);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Bmp_Start_Sel);
+            this.Controls.Add(this.Bmp_Col_Sel);
+            this.Controls.Add(this.Bmp_End_Sel);
+            this.Controls.Add(this.Font_Height_Sel);
+            this.Controls.Add(this.Font_Width_Sel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.OpenButton);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Bitmap2Font";
+            this.Text = "Bitmap2Font";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Font_Width_Sel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Font_Height_Sel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Bmp_End_Sel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Bmp_Col_Sel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Bmp_Start_Sel)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button OpenButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.NumericUpDown Font_Width_Sel;
+        private System.Windows.Forms.NumericUpDown Font_Height_Sel;
+        private System.Windows.Forms.NumericUpDown Bmp_End_Sel;
+        private System.Windows.Forms.NumericUpDown Bmp_Col_Sel;
+        private System.Windows.Forms.NumericUpDown Bmp_Start_Sel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button PrevButton;
+        private System.Windows.Forms.Button GenButton;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
