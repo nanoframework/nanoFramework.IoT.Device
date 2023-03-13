43b79c3849383f5dbc0c50b7df5c01a65f0b3061
jacquesoberto <127383259+jacquesoberto@users.noreply.github.com>
2023-03-13 15:36:46 +0100

    [SSD1306] Added additional fonts  (#744)
    Co-authored-by: Laurent Ellerbach <laurelle@microsoft.com>

---
+++ devices/Ssd13xx/Bitmap2Font/Program.cs
@@ -0,0 +1,30 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Windows.Forms;
+
+namespace Bitmap2Font
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            if (Environment.OSVersion.Version.Major >= 6)
+            {
+                SetProcessDPIAware();
+            }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Bitmap2Font());
+        }
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
+    }
+}
