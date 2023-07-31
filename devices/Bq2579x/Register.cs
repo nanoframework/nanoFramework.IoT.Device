d322de9bb0febf00b939ab6776fd954cc5b37232
José Simões <jose.simoes@eclo.solutions>
2023-07-31 17:49:05 +0100

    Add binding for TI BQ25798 and BQ25792 (#804)
    Co-authored-by: António Fagundes <antonio.fagundes@eclo.solutions>
    Co-authored-by: Laurent Ellerbach <laurelle@microsoft.com>

---
+++ devices/Bq2579x/Register.cs
@@ -0,0 +1,31 @@
+﻿////
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+////
+
+namespace Iot.Device.Bq2579x
+{
+    // refer to Table 9-12. I2C Registers in device datasheet for register definitions
+    internal enum Register : byte
+    {
+        REG00_Minimal_System_Voltage = 0x01,
+        REG01_Charge_Voltage_Limit = 0x01,
+        REG03_Charge_Current_Limit = 0x03,
+        REG05_Input_Voltage_Limit = 0x05,
+        REG06_Input_Current_Limit = 0x06,
+        REG08_Precharge_Control = 0x08,
+        REG10_Charger_Control_1 = 0x10,
+        REG15_Temperatue_Control = 0x16,
+        REG1B_Charger_Status_0 = 0x1B,
+        REG1C_Charger_Status_1 = 0x1C,
+        REG1D_Charger_Status_2 = 0x1D,
+        REG2E_ADC_Control = 0x2E,
+        REG35_VBUS_ADC = 0x35,
+        REG37_VAC1_ADC = 0x37,
+        REG39_VAC2_ADC = 0x39,
+        REG3B_VBAT_ADC = 0x3B,
+        REG3D_VSYS_ADC = 0x3D,
+        REG41_TDIE_ADC = 0x41,
+        REG48_Part_Information = 0x48,
+    }
+}
---
+++ devices/Bq2579x/ThermalShutdownThreshold.cs
@@ -0,0 +1,33 @@
+﻿////
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+////
+
+namespace Iot.Device.Bq2579x
+{
+    /// <summary>
+    /// Thermal shutdown threshold for temperature control.
+    /// </summary>
+    public enum ThermalShutdownThreshold
+    {
+        /// <summary>
+        /// Threshold value 150°C.
+        /// </summary>
+        Temp150C = 0,
+
+        /// <summary>
+        /// Threshold value 130°C.
+        /// </summary>
+        Temp130C = 1,
+
+        /// <summary>
+        /// Threshold value 120°C.
+        /// </summary>
+        Temp120C = 2,
+
+        /// <summary>
+        /// Threshold value 85°C.
+        /// </summary>
+        Temp85C = 3,
+    }
+}
