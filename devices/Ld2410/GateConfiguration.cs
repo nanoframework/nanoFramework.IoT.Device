16a8080c5257aa13ad9248de7d9f5c9416669eb9
MrCSharp <rafael.yousuf@outlook.com>
2023-10-31 22:05:36 +1100

    style: re-enable StyleCop for GateConfiguration class

--- devices/Ld2410/GateConfiguration.cs
+++ devices/Ld2410/GateConfiguration.cs
@@ -15,17 +15,17 @@ namespace Iot.Device.Ld2410
         /// <summary>
         /// The distance, in centimeters, per each radar distance gate.
         /// </summary>
-        public readonly static Length DistancePerGateInCm = Length.FromCentimeters(75);
+        public static readonly Length DistancePerGateInCm = Length.FromCentimeters(75);
 
-		private Ratio _restSensitivity;
-
-		/// <summary>
-		/// Gets the gate number of this configuration instance.
-		/// </summary>
-		public byte Gate { get; }
+        private Ratio _restSensitivity;
 
         /// <summary>
-        /// The motion sensitivity threshold. Any target with a movement "energy" greater than the specified range will be detected, otherwise it will be ignored.
+        /// Gets the gate number of this configuration instance.
+        /// </summary>
+        public byte Gate { get; }
+
+        /// <summary>
+        /// Gets or sets the motion sensitivity threshold. Any target with a movement "energy" greater than the specified range will be detected, otherwise it will be ignored.
         /// Setting a gate's sensitivity to 100 will effectively disable detection of movement within that distance gate.
         /// This can enable fine control over the range of distance to detect movements in. 
         /// For example, All gates except 4 and 7 are set to 100. This means movement will only be detected in gates 4 and 5 (between 3 meters and 5.25 meters from the radar).
@@ -33,36 +33,36 @@ namespace Iot.Device.Ld2410
         /// <remarks>There is no defined unit of measurement for this and should be treated as a percentage. It is recommended to experiment with the values to find the right threshold for the use-case at hand.</remarks>
         public Ratio MotionSensitivity { get; set; }
 
-		/// <summary>
-		/// The resting sensitivity threshold. Any target without movement "energy" (resting) greater than the specified range will be detected, otherwise it will be ignored.
-		/// Setting a gate's sensitivity to 100 will effectively disable detection of resting targets within that distance gate.
-		/// This can enable fine control over the range of distance to detect resting targets in. 
-		/// For example, All gates except 4 and 7 are set to 100. This means resting targets will only be detected in gates 4 and 5 (between 3 meters and 5.25 meters from the radar).
-		/// </summary>
-		/// <remarks>There is no defined unit of measurement for this and should be treated as a percentage. It is recommended to experiment with the values to find the right threshold for the use-case at hand.</remarks>
-		/// <exception cref="InvalidOperationException">Cannot set this value for Gate 0 and 1.</exception>
-		public Ratio RestSensitivity 
+        /// <summary>
+        /// Gets or sets the resting sensitivity threshold. Any target without movement "energy" (resting) greater than the specified range will be detected, otherwise it will be ignored.
+        /// Setting a gate's sensitivity to 100 will effectively disable detection of resting targets within that distance gate.
+        /// This can enable fine control over the range of distance to detect resting targets in. 
+        /// For example, All gates except 4 and 7 are set to 100. This means resting targets will only be detected in gates 4 and 5 (between 3 meters and 5.25 meters from the radar).
+        /// </summary>
+        /// <remarks>There is no defined unit of measurement for this and should be treated as a percentage. It is recommended to experiment with the values to find the right threshold for the use-case at hand.</remarks>
+        /// <exception cref="InvalidOperationException">Cannot set this value for Gate 0 and 1.</exception>
+        public Ratio RestSensitivity 
         { 
             get => _restSensitivity; 
             set
             {
-				if (Gate == 0 || Gate == 1)
-				{
-					throw new InvalidOperationException();
-				}
+                if (Gate == 0 || Gate == 1)
+                {
+                    throw new InvalidOperationException();
+                }
 
-				_restSensitivity = value;
-			}
+                _restSensitivity = value;
+            }
         }
 
-		/// <summary>
-		/// Gets the detection distance of this gate.
-		/// </summary>
-		public Length DetectionDistance
+        /// <summary>
+        /// Gets the detection distance of this gate.
+        /// </summary>
+        public Length DetectionDistance
             => Length.FromCentimeters(Gate * DistancePerGateInCm.Centimeters);
 
         /// <summary>
-        /// Initializes a new instance of <see cref="GateConfiguration"/> with the specified gate number.
+        /// Initializes a new instance of the <see cref="GateConfiguration"/> class with the specified gate number.
         /// </summary>
         /// <param name="gate">The gate number to initialize the <see cref="GateConfiguration"/> class for.</param>
         public GateConfiguration(byte gate)
