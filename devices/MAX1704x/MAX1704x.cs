1841523886661b15f89e25f99f19ac6ed20c127a
Marcin Torba <torbamarcin@gmail.com>
2023-12-28 19:13:39 +0100

    Initial

---
+++ devices/MAX1704x/MAX1704x.cs
@@ -0,0 +1,17 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Device.Gpio;
+using UnitsNet.Units;
+
+namespace Iot.Device.MAX1704x
+{
+    /// <summary>
+    /// Class for controlling A4988 stepper motor driver.
+    /// </summary>
+    public class MAX1704x
+    {
+    
+    }
+}
---
+++ devices/MAX1704x/samples/Program.cs
@@ -0,0 +1,7 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+
+// Pinout for MCU please adapt depending on your MCU
+// Any regular GPIO will work
