2f48946eb9d6b190e9bb963e044a8a04bdd7ffee
José Simões <jose.simoes@eclo.solutions>
2024-03-01 20:45:18 +0000

    [Hts221] Miscellaneous improvements (#994)

--- devices/Hts221/samples/Program.cs
+++ devices/Hts221/samples/Program.cs
@@ -1,16 +1,11 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
-using System.Threading;
-using System.Device.I2c;
 using Iot.Device.Common;
 using Iot.Device.Hts221;
-using UnitsNet;
+using System.Device.I2c;
 using System.Diagnostics;
-
-// I2C address on SenseHat board
-const int I2cAddress = 0x5F;
+using System.Threading;
 
 //////////////////////////////////////////////////////////////////////
 // when connecting to an ESP32 device, need to configure the I2C GPIOs
@@ -24,17 +19,18 @@ while (true)
     var tempValue = th.Temperature;
     var humValue = th.Humidity;
 
-    Debug.WriteLine($"Temperature: {tempValue.DegreesCelsius:0.#}\u00B0C");
-    Debug.WriteLine($"Relative humidity: {humValue:0.#}%");
+    Debug.WriteLine($"Temperature: {tempValue.DegreesCelsius:F1}\u00B0C");
+    Debug.WriteLine($"Relative humidity: {humValue.Percent:F1}%");
 
     // WeatherHelper supports more calculations, such as saturated vapor pressure, actual vapor pressure and absolute humidity.
-    Debug.WriteLine($"Heat index: {WeatherHelper.CalculateHeatIndex(tempValue, humValue).DegreesCelsius:0.#}\u00B0C");
-    Debug.WriteLine($"Dew point: {WeatherHelper.CalculateDewPoint(tempValue, humValue).DegreesCelsius:0.#}\u00B0C");
+    Debug.WriteLine($"Heat index: {WeatherHelper.CalculateHeatIndex(tempValue, humValue).DegreesCelsius:F1}\u00B0C");
+    Debug.WriteLine($"Dew point: {WeatherHelper.CalculateDewPoint(tempValue, humValue).DegreesCelsius:F1}\u00B0C");
+
     Thread.Sleep(1000);
 }
 
 I2cDevice CreateI2cDevice()
 {
-    I2cConnectionSettings settings = new(1, I2cAddress);
+    I2cConnectionSettings settings = new(1, Hts221.DefaultI2cAddress);
     return I2cDevice.Create(settings);
 }
