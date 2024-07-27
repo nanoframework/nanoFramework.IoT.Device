2ce6d3093aa1bdaea0531249f1516197c5d9f360
Marcin Torba <torbamarcin@gmail.com>
2024-07-27 13:41:00 +0200

    Remove Atgm336h project files
    Deleted the entire Atgm336h project along with its associated files, including source code, sample programs, package configurations, and documentation. This cleanup indicates the removal of the Atgm336h module and its dependencies from the repository.

--- devices/Atgm336h/samples/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Iot.Device.Atgm336h.Samples")]
-[assembly: AssemblyCompany("nanoFramework Contributors")]
-[assembly: AssemblyCopyright("Copyright(c).NET Foundation and Contributors")]
-
-[assembly: ComVisible(false)]
