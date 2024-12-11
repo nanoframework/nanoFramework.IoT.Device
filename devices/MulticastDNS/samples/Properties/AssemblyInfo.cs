dda2093cd6f18df09abc146c0b2406ad667f4df0
Klaus Vancamelbeke <99177064+KlausVcb@users.noreply.github.com>
2024-12-11 11:54:27 +0100

    Update devices/MulticastDNS/samples/Properties/AssemblyInfo.cs
    Co-authored-by: Laurent Ellerbach <laurelle@microsoft.com>

--- devices/MulticastDNS/samples/Properties/AssemblyInfo.cs
+++ devices/MulticastDNS/samples/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Iot.Device.MulticastDNS.sample")]
+[assembly: AssemblyTitle("Iot.Device.MulticastDns.sample")]
 [assembly: AssemblyCompany("nanoFramework Contributors")]
 [assembly: AssemblyCopyright("Copyright(c).NET Foundation and Contributors")]
 
