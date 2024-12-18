8ad910c7be899809964ec9763f356b9626b9061f
Klaus Vancamelbeke <klaus.vancamelbek@gmail.com>
2024-12-18 12:08:34 +0100

    New library System.Buffers.Helpers to support both Big and Little Endian BitConverters

---
+++ devices/System.Buffers.Helpers/BitConverter/LittleEndianBitConverter.cs
@@ -0,0 +1,226 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Buffers.Binary;
+
+namespace Iot.Device.MulticastDns.Package
+{
+    /// <summary>
+    /// The <see cref="LittleEndianBitConverter"/> class offers methods to convert to and from a byte[] in a Little Endian manner.
+    /// </summary>
+    public sealed class LittleEndianBitConverter : EndianBitConverter
+    {
+        /// <summary>
+        /// Indicates if this converter instance is Little Endian.
+        /// </summary>
+        public override bool IsLittleEndian => true;
+
+        /// <summary>
+        /// Returns a byte[] representation of a <see cref="char"/>.
+        /// </summary>
+        /// <param name="value">The value to convert.</param>
+        /// <returns>The byte[] with the converted value.</returns>
+        public override byte[] GetBytes(char value)
+        {
+            byte[] result = new byte[2];
+            BinaryPrimitives.WriteInt16LittleEndian(result, (short)value);
+            return result;
+        }
+
+        /// <summary>
+        /// Returns a byte[] representation of a <see cref="short"/>.
+        /// </summary>
+        /// <param name="value">The value to convert.</param>
+        /// <returns>The byte[] with the converted value.</returns>
+        public override byte[] GetBytes(short value)
+        {
+            byte[] result = new byte[2];
+            BinaryPrimitives.WriteInt16LittleEndian(result, value);
+            return result;
+        }
+
+        /// <summary>
+        /// Returns a byte[] representation of a <see cref="int"/>.
+        /// </summary>
+        /// <param name="value">The value to convert.</param>
+        /// <returns>The byte[] with the converted value.</returns>
+        public override byte[] GetBytes(int value)
+        {
+            byte[] result = new byte[4];
+            BinaryPrimitives.WriteInt32LittleEndian(result, value);
+            return result;
+        }
+
+        /// <summary>
+        /// Returns a byte[] representation of a <see cref="long"/>.
+        /// </summary>
+        /// <param name="value">The value to convert.</param>
+        /// <returns>The byte[] with the converted value.</returns>
+        public override byte[] GetBytes(long value)
+        {
+            byte[] result = new byte[8];
+            BinaryPrimitives.WriteInt64LittleEndian(result, value);
+            return result;
+        }
+
+        /// <summary>
+        /// Returns a byte[] representation of a <see cref="ushort"/>.
+        /// </summary>
+        /// <param name="value">The value to convert.</param>
+        /// <returns>The byte[] with the converted value.</returns>
+        public override byte[] GetBytes(ushort value)
+        {
+            byte[] result = new byte[2];
+            BinaryPrimitives.WriteUInt16LittleEndian(result, value);
+            return result;
+        }
+
+        /// <summary>
+        /// Returns a byte[] representation of a <see cref="uint"/>.
+        /// </summary>
+        /// <param name="value">The value to convert.</param>
+        /// <returns>The byte[] with the converted value.</returns>
+        public override byte[] GetBytes(uint value)
+        {
+            byte[] result = new byte[4];
+            BinaryPrimitives.WriteUInt32LittleEndian(result, value);
+            return result;
+        }
+
+        /// <summary>
+        /// Returns a byte[] representation of a <see cref="ulong"/>.
+        /// </summary>
+        /// <param name="value">The value to convert.</param>
+        /// <returns>The byte[] with the converted value.</returns>
+        public override byte[] GetBytes(ulong value)
+        {
+            byte[] result = new byte[8];
+            BinaryPrimitives.WriteUInt64LittleEndian(result, value);
+            return result;
+        }
+
+        /// <summary>
+        /// Returns a byte[] representation of a <see cref="float"/>.
+        /// </summary>
+        /// <param name="value">The value to convert.</param>
+        /// <returns>The byte[] with the converted value.</returns>
+        public override byte[] GetBytes(float value)
+        {
+            byte[] result = new byte[4];
+            BinaryPrimitives.WriteSingleLittleEndian(result, value);
+            return result;
+        }
+
+        /// <summary>
+        /// Returns a byte[] representation of a <see cref="double"/>.
+        /// </summary>
+        /// <param name="value">The value to convert.</param>
+        /// <returns>The byte[] with the converted value.</returns>
+        public override byte[] GetBytes(double value)
+        {
+            byte[] result = new byte[8];
+            BinaryPrimitives.WriteDoubleLittleEndian(result, value);
+            return result;
+        }
+
+        /// <summary>
+        /// Converts a <see cref="SpanByte"/> to a <see cref="char"/>.
+        /// </summary>
+        /// <param name="value">The <see cref="SpanByte"/> that points to the data to convert.</param>
+        /// <param name="index">The index where the data can be found.</param>
+        /// <returns>The converted data.</returns>
+        public override char ToChar(SpanByte value, int index = 0)
+        {
+            return unchecked((char)BinaryPrimitives.ReadInt16LittleEndian(value.Slice(index, 2)));
+        }
+
+        /// <summary>
+        /// Converts a <see cref="SpanByte"/> to a <see cref="short"/>.
+        /// </summary>
+        /// <param name="value">The <see cref="SpanByte"/> that points to the data to convert.</param>
+        /// <param name="index">The index where the data can be found.</param>
+        /// <returns>The converted data.</returns>
+        public override short ToInt16(SpanByte value, int index = 0)
+        {
+            return BinaryPrimitives.ReadInt16LittleEndian(value.Slice(index, 2));
+        }
+
+        /// <summary>
+        /// Converts a <see cref="SpanByte"/> to a <see cref="int"/>.
+        /// </summary>
+        /// <param name="value">The <see cref="SpanByte"/> that points to the data to convert.</param>
+        /// <param name="index">The index where the data can be found.</param>
+        /// <returns>The converted data.</returns>
+        public override int ToInt32(SpanByte value, int index = 0)
+        {
+            return BinaryPrimitives.ReadInt32LittleEndian(value.Slice(index, 4));
+        }
+
+        /// <summary>
+        /// Converts a <see cref="SpanByte"/> to a <see cref="long"/>.
+        /// </summary>
+        /// <param name="value">The <see cref="SpanByte"/> that points to the data to convert.</param>
+        /// <param name="index">The index where the data can be found.</param>
+        /// <returns>The converted data.</returns>
+        public override long ToInt64(SpanByte value, int index = 0)
+        {
+            return BinaryPrimitives.ReadInt64LittleEndian(value.Slice(index, 8));
+        }
+
+        /// <summary>
+        /// Converts a <see cref="SpanByte"/> to a <see cref="ushort"/>.
+        /// </summary>
+        /// <param name="value">The <see cref="SpanByte"/> that points to the data to convert.</param>
+        /// <param name="index">The index where the data can be found.</param>
+        /// <returns>The converted data.</returns>
+        public override ushort ToUInt16(SpanByte value, int index = 0)
+        {
+            return BinaryPrimitives.ReadUInt16LittleEndian(value.Slice(index, 2));
+        }
+
+        /// <summary>
+        /// Converts a <see cref="SpanByte"/> to a <see cref="uint"/>.
+        /// </summary>
+        /// <param name="value">The <see cref="SpanByte"/> that points to the data to convert.</param>
+        /// <param name="index">The index where the data can be found.</param>
+        /// <returns>The converted data.</returns>
+        public override uint ToUInt32(SpanByte value, int index = 0)
+        {
+            return BinaryPrimitives.ReadUInt32LittleEndian(value.Slice(index, 4));
+        }
+
+        /// <summary>
+        /// Converts a <see cref="SpanByte"/> to a <see cref="ulong"/>.
+        /// </summary>
+        /// <param name="value">The <see cref="SpanByte"/> that points to the data to convert.</param>
+        /// <param name="index">The index where the data can be found.</param>
+        /// <returns>The converted data.</returns>
+        public override ulong ToUInt64(SpanByte value, int index = 0)
+        {
+            return BinaryPrimitives.ReadUInt64LittleEndian(value.Slice(index, 8));
+        }
+
+        /// <summary>
+        /// Converts a <see cref="SpanByte"/> to a <see cref="float"/>.
+        /// </summary>
+        /// <param name="value">The <see cref="SpanByte"/> that points to the data to convert.</param>
+        /// <param name="index">The index where the data can be found.</param>
+        /// <returns>The converted data.</returns>
+        public override float ToSingle(SpanByte value, int index = 0)
+        {
+            return BinaryPrimitives.ReadSingleLittleEndian(value.Slice(index, 4));
+        }
+
+        /// <summary>
+        /// Converts a <see cref="SpanByte"/> to a <see cref="double"/>.
+        /// </summary>
+        /// <param name="value">The <see cref="SpanByte"/> that points to the data to convert.</param>
+        /// <param name="index">The index where the data can be found.</param>
+        /// <returns>The converted data.</returns>
+        public override double ToDouble(SpanByte value, int index = 0)
+        {
+            return BinaryPrimitives.ReadDoubleLittleEndian(value.Slice(index, 8));
+        }
+    }
+}
---
+++ devices/System.Buffers.Helpers/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("System.Buffers.Helpers")]
+[assembly: AssemblyCompany("nanoFramework Contributors")]
+[assembly: AssemblyCopyright("Copyright(c).NET Foundation and Contributors")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+/////////////////////////////////////////////////////////////////
+// This attribute is mandatory when building Interop libraries //
+// update this whenever the native assembly signature changes  //
+[assembly: AssemblyNativeVersion("0.0.0.0")]
+/////////////////////////////////////////////////////////////////
