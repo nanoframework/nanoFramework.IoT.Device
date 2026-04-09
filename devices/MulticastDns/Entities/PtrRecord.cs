73311e5f55c2bcd257c947db2df986f277f96dd7
Cory Charlton <CoryCharlton@users.noreply.github.com>
2026-04-09 00:59:05 -0700

    [MulticastDns] Use ClassInternet for PtrRecord per RFC 6762 §10.2
    PTR records are shared records in mDNS service discovery. RFC 6762 §10.2
    explicitly prohibits setting the cache-flush bit on shared records, as
    doing so would cause peers to delete all other responders' PTR records
    for the same service type. Default ResourceClass to ClassInternet (0x0001)
    instead of ClassInternetCacheFlush (0x8001).

--- devices/MulticastDns/Entities/PtrRecord.cs
+++ devices/MulticastDns/Entities/PtrRecord.cs
@@ -17,7 +17,7 @@ namespace Iot.Device.MulticastDns.Entities
         /// <param name="domain">The domain this Record is about.</param>
         /// <param name="targetDomain">The targetDomain which points to the domain.</param>
         /// <param name="ttl">The TTL of this resource.</param>
-        public PtrRecord(string domain, string targetDomain, int ttl = 2000) : base(domain, DnsResourceType.PTR, ttl)
+        public PtrRecord(string domain, string targetDomain, int ttl = 2000) : base(domain, DnsResourceType.PTR, ttl, ClassInternet)
             => Target = targetDomain;
 
         internal PtrRecord(PacketParser packet, string domain, int ttl, ushort rrClass) : base(domain, DnsResourceType.PTR, ttl, rrClass)
